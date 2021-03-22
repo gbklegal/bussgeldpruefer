ce822d565346af8b4b8be7d660cc06faf3871949
Alex Schieder <alex.schieder@posteo.net>
2021-03-22 14:34:53 +0100

    Add views Home, About

---
+++ web/src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>About</h1>
+    <section class="grid grid-cols-2">
+        <div class="max-w-1/2 h-20"></div>
+        <div class="max-w-1/2 h-20 bg-primary"></div>
+    </section>
+</template>
---
+++ web/src/views/Home.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="home">
+    <header class="lg:flex lg:justify-between">
+        <div class="first-half my-20 lg:max-w-1/2 lg:grid">
+            <div></div>
+            <div class="lg:container-half-left">
+                <h1 class="text-4xl md:text-5xl mb-6">Du bist nur 2 Minuten davon entfernt, deinen Bußgeldbescheid anzufechten.</h1>
+                <p class="text-xl">Hol dir jetzt die BussgeldPrüfer App, um eine unverbindliche Ersteinschätzung zu erhalten – komplett kostenlos.</p>
+            </div>
+        </div>
+
+        <div class="secondhalf bg-primary p-5 lg:max-w-1/2 my-2">
+            <div class="container-half-right grid">
+                <div>
+                    <div class="flex flex-col sm:flex-row justify-center text-white">
+                        <img class="w-56 self-center" src="../assets/img/iphone-x-splashscreen.webp" alt="BussgeldPrüfer App auf einem Handy.">
+                        <ul role="list" class="text-lg my-10 mb-3 self-center list-disc lg:ml-10 sm:ml-20">
+                        <li class="mb-5">Kostenlose Ersteinschätzung</li>
+                        <li class="mb-5">in 90 Sekunden zum Ergebnis</li>
+                        <li>Digital ausführbar</li>
+                        </ul>
+                    </div>
+                    <p class="text-center mt-4 text-white">überall - digital - verbunden</p>
+                </div>
+                <div></div>
+            </div>
+        </div>
+    </header>
+
+    <CTA title="Bußgeldbescheid erhalten?" subtitle="Punkte in Flensburg? Führerschein weg? Fahrverbot? MPU-Test? Geblitzt?" />
+
+    <section class="">
+      <div class="container flex flex-col md:flex-row justify-center md:justify-between">
+        <div class="md:max-w-1/2">
+          <h2 class="font-medium mb-5 text-3xl md:text-4xl">Mit dem BussgeldPrüfer fährst du richtig:</h2>
+          <ul role="list" class="w-max mb-5">
+            <li>Unverbindliche Ersteinschätzung in 90 Sekunden</li>
+            <li>Kostenlose telefonische Beratung</li>
+            <li>Schnelle Überprüfung Deiner Erfolgsaussichten</li>
+          </ul>
+        </div>
+
+        <div class="md:max-w-1/2 relative h-52 w-96">
+          <div class="circle-one bg-white grid place-items-center absolute text-center rounded-full h-32 w-32 border-2 border-secondary">
+            <p>Kostenlose Erstberatung</p>
+          </div>
+          <div class="circle-two bg-white grid place-items-center absolute text-center rounded-full h-32 w-32 border-2 border-secondary">
+            <p>Anwaltliche Prüfung</p>
+          </div>
+          <div class="circle-three bg-white grid place-items-center absolute text-center rounded-full h-32 w-32 border-2 border-secondary">
+            <p>in 24 Std. Rückmeldung</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <Steps />
+
+    <section class="success lg:flex lg:justify-between">
+        <div class="first-half my-20 lg:max-w-1/2 lg:grid mb-20 lg:mb-0">
+            <div></div>
+            <div class="lg:container-half-left">
+                <h2 class="font-medium mb-10 text-3xl md:text-4xl">
+                Maximiere deine Erfolgsaussichten
+                </h2>
+                <p class="">Mit dem BussgeldPrüfer erhöhst du deine Chance erfolgreich gegen deinen Bußgeldbescheid vorzugehen. Klicke jetzt auf “Prüfung Bußgeldbescheid” um deinen Bußgeldbescheid anzufechten.</p>
+            </div>
+        </div>
+
+      <div class="secondhalf bg-primary p-5 lg:max-w-1/2 my-2 text-secondary">
+        <div class="container-half-right grid">
+            <div class="sm:flex-row-reverse flex flex-col justify-center lg:pr-10">
+                <img class="w-56 mx-auto" src="../assets/img/iphone-x-splashscreen.webp" alt="BussgeldPrüfer App auf einem Handy.">
+                <ul role="list" class="mt-10 mb-3 mx-auto lg:mr-5 sm:self-center">
+                <li class="mb-5">Kompetente<br>Beratung</li>
+                <li class="mb-5">Vollständige<br>Entlastung</li>
+                <li>Schnell &amp;<br>Unkompliziert</li>
+                </ul>
+            </div>
+            <div></div>
+        </div>
+      </div>
+    </section>
+
+    <CTA title="Kostenlose Prüfung ob sich ein Einspruch gegen Deinen Bußgeldbescheid lohnt." />
+
+    <!-- TODO: Blog / last 3 Blogposts -->
+    <!-- TODO: Clientfeedback -->
+
+    <SocialMedia />
+    <Warrants />
+
+  </div>
+</template>
+
+<script>
+import CTA from '../components/BasicCTA.vue'
+import Steps from '../components/BasicSteps.vue'
+import SocialMedia from '../components/BasicSocialMedia.vue'
+import Warrants from '../components/BasicWarrants.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    CTA,
+    Steps,
+    SocialMedia,
+    Warrants,
+  }
+}
+</script>
+
+<style scoped>
+  /*li:before {
+    content: "";
+    padding-right: 0.2em;
+  }*/
+
+  /*.success ul {
+    list-style-image: url("../assets/img/check-mark.svg");
+  }*/
+
+  .success ul li:before {
+    content: "";
+  }
+
+  .success ul li {
+    padding-left: 2.4em;
+    background: url("../assets/img/check-mark.svg") no-repeat 0 50%;
+  }
+
+  .circle-two {
+    left: 108px;
+    top: 60px;
+    z-index: 1;
+  }
+
+  .circle-three {
+    left: 210px;
+    top: 20px;
+    z-index: 2;
+  }
+
+@media (min-width: 1024px) {
+  .first-half {
+    grid-template-columns: calc(50vw - 512px) 512px;
+  }
+
+  .second-half {
+    grid-template-columns: 512px calc(50vw - 512px);
+  }
+}
+
+@media (min-width: 1280px) {
+  .first-half {
+    grid-template-columns: calc(50vw - 640px) 640px;
+  }
+
+  .second-half {
+    grid-template-columns: 640px calc(50vw - 640px);
+  }
+}
+</style>
