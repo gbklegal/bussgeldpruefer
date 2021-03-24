9509d4b02ecea20af25b26f747b5bc5b5ade6660
Alex Schieder <alex.schieder@posteo.net>
2021-03-24 10:21:59 +0100

    Add empty Order view

--- web/src/router.js
+++ web/src/router.js
@@ -1,12 +1,14 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import Order from './views/Order.vue'
 
 const history = createWebHistory()
 
 const routes = [
     { path: '/', component: Home },
     { path: '/about', component: About },
+    { path: '/bussgeldbescheid-pruefen', component: Order },
 ]
 
 const router = createRouter({ history, routes })
