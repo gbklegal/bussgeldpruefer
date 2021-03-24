120046150929698121ddd18871a41f7339ebd562
Alex Schieder <alex.schieder@posteo.net>
2021-03-24 10:21:47 +0100

    Add test to BasicCTA component And delete todo

--- web/src/components/BasicCTA.vue
+++ web/src/components/BasicCTA.vue
@@ -2,7 +2,6 @@
     <section class="bg-primary">
         <div class="container text-center text-white">
             <h2 class="mb-10 text-2xl md:text-3xl text-secondary">{{ title }}</h2>
-            <!-- TODO: only show p if prop is submitted -->
             <p class="mb-10">{{ subtitle }}</p>
             <router-link class="btn-primary text-white" to="/bussgeldbescheid-pruefen">
                 Bussgeldbescheid Prüfung
