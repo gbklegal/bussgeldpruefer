baf436d12410dfe0b65aa55f64072c7d027a01b5
Alex Schieder <alex.schieder@posteo.net>
2021-03-25 13:58:00 +0100

    Add content and style to Blog.vue + component Component: BlogSearchForm; Logic is missing!

--- web/src/views/Blog.vue
+++ web/src/views/Blog.vue
@@ -1,3 +1,29 @@
 <template>
-    <h1>Bussgeldratgeber</h1>
+    <div class="container">
+        <h1 class="text-secondary">Bussgeldratgeber</h1>
+        <p class="text-xl">Themenübersicht</p>
+        <BlogSearch />
+        <div class="mt-10">
+            <BlogPreview imgsrc="/src/assets/img/alcohol.jpg" imgalt="Bild mit Alkohol" blogtitle="Führerscheinverlust bei Alkoholkonsum" previewtext="Die Tempo-30er-Zone erfüllt drei grundlegende Funktionen. So soll die 30-Zone die Verkehrssicherheit erhöhen. Aufgrund eines Verkehrssicherheitsprogrammes führte Berlin vor fast allen Schulen und Kitas die 30er Zone ein." />
+
+            <BlogPreview imgsrc="/src/assets/img/alcohol.jpg" imgalt="Bild mit Alkohol" blogtitle="Führerscheinverlust bei Alkoholkonsum" previewtext="Die Tempo-30er-Zone erfüllt drei grundlegende Funktionen. So soll die 30-Zone die Verkehrssicherheit erhöhen. Aufgrund eines Verkehrssicherheitsprogrammes führte Berlin vor fast allen Schulen und Kitas die 30er Zone ein." />
+
+            <BlogPreview imgsrc="/src/assets/img/alcohol.jpg" imgalt="Bild mit Alkohol" blogtitle="Führerscheinverlust bei Alkoholkonsum" previewtext="Die Tempo-30er-Zone erfüllt drei grundlegende Funktionen. So soll die 30-Zone die Verkehrssicherheit erhöhen. Aufgrund eines Verkehrssicherheitsprogrammes führte Berlin vor fast allen Schulen und Kitas die 30er Zone ein." />
+
+            <BlogPreview imgsrc="/src/assets/img/alcohol.jpg" imgalt="Bild mit Alkohol" blogtitle="Führerscheinverlust bei Alkoholkonsum" previewtext="Die Tempo-30er-Zone erfüllt drei grundlegende Funktionen. So soll die 30-Zone die Verkehrssicherheit erhöhen. Aufgrund eines Verkehrssicherheitsprogrammes führte Berlin vor fast allen Schulen und Kitas die 30er Zone ein." />
+        </div>
+
+        <button class="btn-primary mt-10 mx-auto block">Mehr laden</button>
+    </div>
 </template>
+<script>
+import BlogPreview from '../components/BlogPreview.vue'
+import BlogSearch from '../components/BlogSearchForm.vue'
+
+export default {
+    components: {
+        BlogPreview,
+        BlogSearch
+    }
+}
+</script>
