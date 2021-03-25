7ab8c76be4fa32076c54b62e746c12e156c450ab
Alex Schieder <alex.schieder@posteo.net>
2021-03-25 11:22:35 +0100

    Fix style Issues BlogPreview

--- web/src/components/BlogPreview.vue
+++ web/src/components/BlogPreview.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="flex flex-col sm:flex-row text-left">
+    <div class="flex flex-col sm:flex-row text-left mt-8">
         <img :src="imgsrc" :alt="imgalt" class="rounded-sm object-cover h-44 w-full">
-        <div class="ml-6">
+        <div class="mt-6 sm:mt-0 sm:ml-6">
             <h3>{{ blogtitle }}</h3>
             <p>{{ previewtext }}</p>
         </div>
