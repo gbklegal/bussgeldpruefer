09ed1ae15ec3e8398c791143c0d0f46119fdffd5
Alex Schieder <alex.schieder@posteo.net>
2021-03-25 13:56:49 +0100

    Change border-width

--- web/src/components/BasicSteps.vue
+++ web/src/components/BasicSteps.vue
@@ -6,15 +6,15 @@
             </h2>
             <div class="md:grid md:grid-cols-3 xl:mx-auto">
                 <div class="mb-10">
-                    <img src="../assets/img/assignment.svg" alt="Icon mit Dokumenten." class="mx-auto border-4 rounded-full border-white w-32 h-32"/>
+                    <img src="../assets/img/assignment.svg" alt="Icon mit Dokumenten." class="mx-auto border-2 rounded-full border-white w-32 h-32"/>
                     <p class="mt-3">Formular ausfüllen &amp; relevante Dokumente schicken</p>
                 </div>
                 <div class="mb-10">
-                    <img src="../assets/img/communication.svg" alt="Icon mit Personen." class="mx-auto border-4 rounded-full border-white w-32 h-32" />
+                    <img src="../assets/img/communication.svg" alt="Icon mit Personen." class="mx-auto border-2 rounded-full border-white w-32 h-32" />
                     <p class="mt-3">Kontaktaufnahme mit Patnerkanzlei &amp; Planung der Verteidigungsstrategie</p>
                 </div>
                 <div class="">
-                    <img src="../assets/img/innovation.svg" alt="Icon mit einer Glühbirne." class="mx-auto border-4 rounded-full border-white w-32 h-32" />
+                    <img src="../assets/img/innovation.svg" alt="Icon mit einer Glühbirne." class="mx-auto border-2 rounded-full border-white w-32 h-32" />
                     <p class="mt-3">Gerichtsurteil &amp; erfolgreicher Fallabschluss</p>
                 </div>
             </div>
