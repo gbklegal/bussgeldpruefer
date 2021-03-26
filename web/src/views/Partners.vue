5a5f57b2bacf41838266d7b3775a29c5efa0c723
Alex Schieder <alex.schieder@posteo.net>
2021-03-26 10:53:14 +0100

    Add content to Partners.vue

--- web/src/views/Partners.vue
+++ web/src/views/Partners.vue
@@ -1,3 +1,49 @@
 <template>
-    <h1>Partners</h1>
+    <div class="container">
+        <h1 class="text-secondary mb-3">Partnerprogramme &amp; Kooperationen</h1>
+        <p class="text-xl mb-12">Werde Partner von BussgeldPrüfer!</p>
+
+        <div class="flex flex-col md:flex-row items-center mb-10">
+            <div class="border-2 rounded-full grid place-items-center text-center border-primary h-44 w-44 mb-6 md:mb-0 md:mr-4">
+                <h2 class="mb-0 text-xl text-secondary">Autohändler, Werkstätten, Versicherungen</h2>
+            </div>
+            <div class="max-w-md">
+                <p class="text-secondary mb-4">Du bist in den folgenden Bereichen tätig? Dann werde Teil des <span class="text-primary">BussgeldPrüfer</span> Teams in drei einfachen Schritten:</p>
+                <ul class="ml-5 list-disc">
+                    <li>Du erhältst einen personalisierten Link mit deinem Logo</li>
+                    <li>Sobald ein Kunde Ärger mit seinem Bußgeldbescheid hat, teilst du deinen personalisierten Link mit ihm.</li>
+                    <li>Für jeden erfolgreichen vermittelten Kunden erhältst Du eine Provision.</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="flex flex-col md:flex-row items-center mb-10">
+            <div class="border-2 rounded-full grid place-items-center text-center border-primary h-44 w-44 mb-6 md:mb-0 md:mr-4">
+                <h2 class="mb-0 text-xl text-secondary">Partneranwalt</h2>
+            </div>
+            <div class="max-w-md">
+                <p class="text-secondary mb-4">Du bist als zugelassener Anwalt tätig? Dann werde Teil des <span class="text-primary">BussgeldPrüfer</span> Teams</p>
+            </div>
+        </div>
+
+        <div>
+            <h2 class="text-secondary mb-3 text-xl">Dein Interesse wurde geweckt? Dann melde dich bei uns!</h2>
+            <p class="text-secondary text-xl mb-6">Wir werden dir schnellstmöglich Rückmeldung geben.</p>
+            <p class="mb-4">Ansprechpartner für Partnerprogramme &amp; Kooperationen:</p>
+
+            <div class="mb-6">
+                <p>Herr Tobias Gussmann</p>
+                <div>
+                    <span class="mr-2">Email:</span>
+                    <a href="mailto:dialog@bussgeldpruefer.de">dialog@bussgeldpruefer.de</a>
+                </div>
+                <div>
+                    <span class="mr-2">Tel:</span>
+                    <a href="tel:091112345678">0911/12345678</a>
+                </div>
+            </div>
+
+            <p>Du willst auf andere Weise mit uns kooperieren? Dann schreibe uns gerne mit dem Stichwort "Kooperation" an.</p>
+        </div>
+    </div>
 </template>
