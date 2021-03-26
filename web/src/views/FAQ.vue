7092fe5d2a163cb6003a78d6c672e3f2a2003756
Alex Schieder <alex.schieder@posteo.net>
2021-03-26 13:21:49 +0100

    Change BasicSearch And update it on every Site it is used

--- web/src/views/FAQ.vue
+++ web/src/views/FAQ.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="container">
         <h1 class="text-secondary">FAQ - Häufig gestellt Fragen</h1>
-        <p class="text-xl">Themenübersicht</p>
         <Search />
         <div class="mt-7">
             <FAQItem faqTitle="Welche Fälle können über BussgeldPrüfer eingereicht werden?" faqText="<p>Über BussgeldPrüfer können Sie Fälle über die häufigsten Themen zu Bußgeldbescheiden wie: Abstand, Anhörungsbogen, Alkohol, Blitzer, Drogen, Flensburg, Geschwindigkeit, Rote Ampel, Rotlichtverstoß, Parken und Halten, Probezeit sowie MPU und Ounkte einreichen</p><p>Unsere Partner- und Fachanwälte im Verkehrsrecht beraten Sie bei allen Bußgeldverfahren aus dem innerdeutschen Straßenverkehr. Zögern Sie nicht auch Bußgeldfälle im Zusammenhang mit einem Verkehrsunfall über BussgeldPrüfer einzureichen.</p>" />
