334b651ea6c73f38726bacff9282757e8864bb21
Alex Schieder <alex.schieder@posteo.net>
2021-03-26 13:07:40 +0100

    Add content to LegalContact

--- web/src/views/LegalContact.vue
+++ web/src/views/LegalContact.vue
@@ -1,3 +1,52 @@
 <template>
-    <h1>Impressum</h1>
+    <div class="container">
+        <h1>Impressum</h1>
+
+        <div class="mb-6">
+            <h2 class="text-xl mb-3">Impressum</h2>
+            <p>GDB L-Tech Unternehmergesellschaft (haftungsbeschränkt)</p>
+            <p>Nordostpark 102a</p>
+            <p>90411 Nürnberg</p>
+            <div>
+                <span class="mr-2">Email:</span>
+                <a href="mailto:dialog@bussgeldpruefer.de">dialog@bussgeldpruefer.de</a>
+            </div>
+            <div>
+                <span class="mr-2">Tel:</span>
+                <a href="tel:091112345678">0911/12345678</a>
+            </div>
+            <div class="mb-3">
+                <span class="mr-2">Internet:</span>
+                <a href="https://xn-bussgeldprfer-5ob.com/">www.bussgeldprüfer.com</a>
+            </div>
+            <p>Die Gesellschaft ist eine Unternehmergesellschaft (haftungsbeschränkt) mit Sitz in Nürnberg.</p>
+            <p>Handelsregister beim Amtsgericht Nürnberg: HRB 33696
+            Umsatzsteuer-Identifikationsnummer: DE310040081
+            </p>
+        </div>
+        <div class="mb-6">
+            <h2 class="text-xl mb-3">Geschäftsführer</h2>
+            <p>Die Gesellschaft wird vertreten durch den Geschäftsführer Tobias Gußmann</p>
+        </div>
+        <div class="mb-6">
+            <h2 class="text-xl mb-3">Verantwortlich für den Inhalt <span class="text-sm">(nach § 55 Abs. 2 RStV)</span></h2>
+            <p>Tobias Gußmann</p>
+        </div>
+        <div class="mb-6">
+            <h2 class="text-xl mb-3">Online-Streitbeilegung</h2>
+            <p>Die Europäische Kommission stellt eine Plattform für die außergerichtliche Online-Streitbeilegung (OS-Plattform) bereit, die unter www.ec.europa.eu/consumers/odr aufrufbar ist. Unsere E-Mail-Adresse finden Sie in unserem Impressum. Wir sind weder verpflichtet noch bereit, an dem Streitschlichtungsverfahren teilzunehmen.</p>
+        </div>
+        <p class="font-bold mb-6">Dieses Impressum gilt auch für die Facebookseite.</p>
+
+        <div class="mb-6">
+            <h2 class="text-xl mb-3">Hinweis zum Leistungsschutzrecht</h2>
+            <p>Ein Zitieren unserer Inhalte und Links auf unsere Seite begrüßen und fördern wir ausdrücklich. Dennoch gilt als Richtlinie: Überschriften plus Anreißer-Texte oder ein Ausschnitt in vergleichbarer Länge dürfen von anderen Webseiten zitiert werden. Suchmaschinen dürfen Textsnippets der Inhalte unserer Seiten in Ergebnislisten darstellen. In diesen Fällen werden wir keine Lizenzgebühren erheben. Eine Übernahme längerer Textpassagen oder sogar ganzer Texte wird nur mit Zustimmung der GDB L-Tech UG (haftungsbeschränkt) möglich sein. In diesem Falle werden Lizenzgebühren erhoben. Gerne können Sie hierzu Kontakt mit uns aufnehmen.</p>
+        </div>
+        <div>
+            <h2 class="text-xl mb-3">Bildnachweise</h2>
+            <p>panthermedia.net</p>
+            <p>fotolia.de</p>
+        </div>
+
+    </div>
 </template>
