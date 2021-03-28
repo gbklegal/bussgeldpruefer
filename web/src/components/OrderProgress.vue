7b966202a1022e22aa900b1e141c3475d9dc7ba9
Alex Schieder <alex.schieder@posteo.net>
2021-03-28 17:56:38 +0200

    Add Order and OrderProgress

---
+++ web/src/components/OrderProgress.vue
@@ -0,0 +1,39 @@
+<template>
+    <label class="text-xs float-right" for="order-progress">{{ text }}</label>
+    <progress id="order-progress" :value="percentage" max="100" class="bg-gray-200 block w-full rounded">{{ percentage }}%</progress>
+</template>
+<script>
+export default {
+    props: {
+        text: String,
+        percentage: Number,
+    }
+}
+</script>
+<style scoped>
+/* Reset default */
+progress[value] {
+    -webkit-appearance: none;
+       -moz-appearance: none;
+            appearance: none;
+    border: none;
+    color: #5CC8C5;
+}
+
+progress[value]::-webkit-progress-bar {
+    /*background-color: #eee;*/
+    background-color: #fff;
+    border-radius: 3px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.25) inset;
+}
+
+progress[value]::-moz-progress-bar {
+    background-color: #183B63;
+    border-radius: 3px;
+}
+
+progress[value]::-webkit-progress-value {
+    background-color: #183B63;
+    border-radius: 3px;
+}
+</style>
