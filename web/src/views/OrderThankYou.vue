c33b6dbde55e28e527144800dbaff9ceb65bc9d2
Alex Schieder <alex.schieder@posteo.net>
2021-03-29 15:25:38 +0200

    Modify ThankYou page Adjust style

--- web/src/views/OrderThankYou.vue
+++ web/src/views/OrderThankYou.vue
@@ -8,12 +8,12 @@
         <p class="text-xl">Dein Antrag wurde erfolgreich eingereicht.</p>
         <img src="/src/assets/img/confetti.svg" class="w-20 my-4 ml-20" alt="Icon mit Konfetti.">
         <p class="text-secondary mt-6 mb-4">Was passiert als Nächstes?</p>
-        <ol class="list-decimal ml-5 mb-6">
-            <li>Du erhältst nun eine E-Mail mit allen wichtigen Daten.</li>
+        <ol class="ml-5 mb-10 relative">
+            <li class="mb-2">Du erhältst nun eine E-Mail mit allen wichtigen Daten.</li>
             <li>Die Anwaltskanzlei meldet sich bei dir in 24 Stunden.(Es entstehen keine Kosten.)</li>
         </ol>
-        <CaseProgress percentage="25" />
-        <p class="mt-6 mb-4">Du hast was vergessen? Kein Problem! Antworte einfach auf die BussgeldPrüfer E-Mail und wir werden uns darum kümmern.</p>
+        <CaseProgress />
+        <p class="mt-10 mb-4">Du hast was vergessen? Kein Problem! Antworte einfach auf die BussgeldPrüfer E-Mail und wir werden uns darum kümmern.</p>
         <a href="mailto:dialog@bussgeldpruefer.de">dialog@bussgeldpruefer.de</a>
 
     </div>
@@ -21,8 +21,6 @@
 <script>   
 import OrderProgress from '../components/OrderProgress.vue'
 import CaseProgress from '../components/OrderCaseProgress.vue'
-
-import { ref } from 'vue'
 
 export default {
     components: {
@@ -39,4 +37,18 @@ export default {
 </script>
 <style lang="scss" scoped>
 
+ol {
+    counter-reset: next-steps;
+}
+
+ol li {
+    counter-increment: next-steps;
+}
+
+ol li::before {
+    content: counter(next-steps) ". ";
+    color: #5CC8C5;
+    position: absolute;
+    left: -1em;
+}
 </style>
