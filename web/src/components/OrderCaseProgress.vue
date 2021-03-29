50fbb098220c45c463cd1003becbc2eccda42f1b
Alex Schieder <alex.schieder@posteo.net>
2021-03-29 15:25:07 +0200

    Modify CaseProgress Component

--- web/src/components/OrderCaseProgress.vue
+++ web/src/components/OrderCaseProgress.vue
@@ -1,5 +1,29 @@
 <template>
-    <progress id="order-progress" :value="percentage" max="100" class="bg-gray-200 block w-full rounded">{{ percentage }}%</progress>
+    <div class="relative">
+        <div class="bg-gray-200 block sm:w-full sm:h-24 rounded shadow-inner h-96 w-24">
+            <div class="sm:w-1/4 h-1/4 w-24 sm:h-24 bg-secondary rounded"></div>
+        </div>
+
+        <div class="absolute w-full h-96 sm:h-auto grid sm:grid-cols-4 -top-1 left-1.5 sm:left-0">
+            <div class="text-center flex sm:flex-col">
+                <img class="w-20 block sm:mx-auto mt-3" src="/src/assets/img/assignment.svg" alt="Icon mit Dokumenten.">
+                <span class="inline-block mt-3 ml-5 sm:ml-0 text-sm">Antrag abgeben</span>
+            </div>
+            <div class="text-center flex sm:flex-col">
+                <img class="w-20 block sm:mx-auto mt-3" src="/src/assets/img/communication.svg" alt="Icon mit Personen.">
+                <span class="inline-block mt-3 ml-5 sm:ml-0 text-sm">Fallbesprechung</span>
+            </div>
+            <div class="text-center flex sm:flex-col">
+                <img class="w-20 block sm:mx-auto mt-3" src="/src/assets/img/innovation.svg" alt="Icon mit Lupe.">
+                <span class="inline-block mt-3 ml-5 sm:ml-0 text-sm">Überprüfung</span>
+            </div>
+            <div class="text-center flex sm:flex-col">
+                <img class="w-20 block sm:mx-auto mt-3" src="/src/assets/img/sentence.svg" alt="Icon mit Richterhammer">
+                <span class="inline-block mt-3 ml-5 sm:ml-0 text-sm">Urteil</span>
+            </div>
+        </div>
+    </div>
+
 </template>
 <script>
 export default {
@@ -9,29 +33,15 @@ export default {
 }
 </script>
 <style scoped>
-/* Reset default */
-progress[value] {
-    -webkit-appearance: none;
-       -moz-appearance: none;
-            appearance: none;
-    border: none; 
-    color: #5CC8C5;
+
+span {
+    line-height: 5rem;
 }
 
-progress[value]::-webkit-progress-bar {
-    /*background-color: #eee;*/
-    background-color: #fff;
-    border-radius: 3px;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.25) inset;
+@media (min-width: 640px) {
+    span {
+        line-height: 1.25rem;
+    }
 }
 
-progress[value]::-moz-progress-bar {
-    background-color: #5CC8C5;
-    border-radius: 3px;
-}
-
-progress[value]::-webkit-progress-value {
-    background-color: #5CC8C5;
-    border-radius: 3px;
-}
 </style>
