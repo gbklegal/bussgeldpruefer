f85dc5369bb36d0201ba5447987063ded9106c4e
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2021-04-01 17:12:46 +0200

    Add profile

--- app/lib/screens/profile.dart
+++ app/lib/screens/profile.dart
@@ -1,48 +1,90 @@
+import 'package:app/functions/newscreen.dart';
+import 'package:app/screens/dummy.dart';
+import 'package:app/widgets/pagetitle.dart';
 import 'package:flutter/material.dart';
 
-import 'package:app/functions/newscreen.dart';
-
-import 'package:app/widgets/appbar.dart';
-
 class ProfileScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text('Profile'),
-          ElevatedButton(
-            child: Text('Login'),
-            onPressed: () => newScreen(
-              context: context,
-              screen: LoginScreen(),
-            ),
+  _padding() {
+    return SizedBox(height: 20.0);
+  }
+
+  _imageTextButton({icon, text, context, screen}) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () => newScreen(
+          context: context,
+          screen: screen,
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: <Widget>[
+              Icon(
+                icon,
+                size: 40,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Text(
+                  text,
+                  style: TextStyle(fontSize: 20),
+                  softWrap: true,
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
-}
 
-class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBarWidget(
-        isHome: false,
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Icon(Icons.close),
-              Text('Close LoginScreen'),
-            ],
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        children: <Widget>[
+          PageTitle('Mein Profil'),
+          Padding(
+            padding: EdgeInsets.fromLTRB(
+              20.0,
+              0.0,
+              20.0,
+              20.0,
+            ),
+            child: Column(
+              children: <Widget>[
+                Text(
+                    'In deinem Profil kannst du auf alle wichtigen Unterlagen jederzeit zugreifen.'),
+                _padding(),
+                _padding(),
+                _imageTextButton(
+                  icon: Icons.email_outlined,
+                  text: 'Online Posteingang',
+                  context: context,
+                  screen: DummyScreen(),
+                ),
+                _padding(),
+                _imageTextButton(
+                  icon: Icons.rotate_right_outlined,
+                  text: 'Überprüfe den aktuellen Status Deines Falls',
+                  context: context,
+                  screen: DummyScreen(),
+                ),
+                _padding(),
+                _imageTextButton(
+                  icon: Icons.cloud_upload_outlined,
+                  text: 'Dokumenten Übersicht',
+                  context: context,
+                  screen: DummyScreen(),
+                ),
+              ],
+            ),
           ),
-          onPressed: () => Navigator.pop(context),
-        ),
+        ],
       ),
     );
   }
