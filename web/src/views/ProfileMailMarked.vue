e0d615722de4fa2bf001a0757c8b9404af2d56a2
Alex Schieder <alex.schieder@posteo.net>
2021-04-06 11:27:22 +0200

    Add content to profile-mail views

---
+++ web/src/views/ProfileMailMarked.vue
@@ -0,0 +1,28 @@
+<template>
+    <h2 class="text-xl inline">Markiert</h2>
+
+    <!-- Message -->
+    <div class="py-5 border-b-2 border-gray-200 flex justify-between">
+        <div class="font-light">
+            <p>Diese Unterlagen fehlen</p>
+            <p>Sehr geehrte Herr Xy, vielen Dank für Interesse. Bla</p>
+        </div>
+        <div>
+            <span>23.12.</span>
+            <button @click="marked = !marked" class="block mx-auto">
+                <img v-if="!marked" class="w-5" src="/src/assets/img/star-outline.svg" alt="Klicke hier um die Nachricht zu markieren.">
+                <img v-if="marked" class="w-5" src="/src/assets/img/star.svg" alt="Klicke hier um die Nachricht nicht mehr zu markieren.">
+            </button>
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data: () => {
+        return {
+            marked: true,
+        }
+    }
+}
+</script>
--- web/src/views/ProfileMailNewMessage.vue
+++ web/src/views/ProfileMailNewMessage.vue
@@ -2,22 +2,47 @@
     <h2 class="text-xl">E-Mail schreiben</h2>
 
     <form @submit.prevent>
-        <div class="border-2 border-gray-200 rounded-sm">
-            <label class="ml-2" for="sendTo">An:</label>
-            <input class="border-transparent" type="text" name="sendTo" id="sendTo">
+        <div class="form-item items-center grid border-2 border-gray-200 rounded-sm">
+            <label class="mx-2" for="sendTo">An:</label>
+            <input v-model="mailTo" class="border-transparent border-2 rounded focus:ring focus:border-secondary focus:ring-secondary focus:ring-opacity-50" type="text" name="sendTo" id="sendTo">
+        </div>
+        <div class="form-item items-center grid border-2 border-t-0 border-gray-200 rounded-sm">
+            <label class="mx-2" for="sendFrom">Von:</label>
+            <input v-model="mailFrom" class="border-transparent border-2 rounded focus:ring focus:border-secondary focus:ring-secondary focus:ring-opacity-50" type="text" name="sendFrom" id="sendFrom">
+        </div>
+        <div class="form-item items-center grid border-2 border-t-0 border-gray-200 rounded-sm">
+            <label class="mx-2" for="subject">Betreff:</label>
+            <input v-model="mailSubject" class="border-transparent border-2 rounded focus:ring focus:border-secondary focus:ring-secondary focus:ring-opacity-50" type="text" name="subject" id="subject">
         </div>
         <div class="border-2 border-t-0 border-gray-200 rounded-sm">
-            <label class="ml-2" for="sendFrom">Von:</label>
-            <input class="border-transparent" type="text" name="sendFrom" id="sendFrom">
-        </div>
-        <div class="border-2 border-t-0 border-gray-200 rounded-sm">
-            <label class="ml-2" for="subject">Betreff:</label>
-            <input class="border-transparent" type="text" name="subject" id="subject">
-        </div>
-        <div class="border-2 border-t-0 border-gray-200 rounded-sm">
-            <textarea class="border-transparent" name="sendText" id="sendText" cols="30" rows="10"></textarea>
+            <textarea v-model="mailBody" class="w-full border-transparent border-2 rounded focus:ring focus:border-secondary focus:ring-secondary focus:ring-opacity-50" name="sendText" id="sendText" cols="30" rows="10"></textarea>
         </div>
         <button class="btn-primary mt-4" type="submit">senden</button>
     </form>
 
+    <!-- debuggeing <p>An: {{mailTo}}</p>
+    <p>Von: {{mailFrom}}</p>
+    <p>Betreff: {{mailSubject}}</p>
+    <p>Body: {{mailBody}}</p> -->
+
 </template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+    setup() {
+        // form data
+        const mailTo = ref('')
+        const mailFrom = ref('')
+        const mailSubject = ref('')
+        const mailBody = ref('')
+    },
+}
+</script>
+
+<style scoped>
+    .form-item {
+        grid-template-columns: auto 1fr;
+    }
+</style>
