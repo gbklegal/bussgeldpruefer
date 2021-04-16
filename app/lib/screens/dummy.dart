7ca61c3d6f5e88c5aa9fba128770ff8c5b5210c8
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2021-04-16 15:27:54 +0200

    Add quick check violation demo

--- app/lib/screens/dummy.dart
+++ app/lib/screens/dummy.dart
@@ -1,7 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animation_progress_bar/flutter_animation_progress_bar.dart';
 
-class DummyScreen extends StatelessWidget {
+enum Violation {
+  alcohol,
+  distance,
+  parking,
+  phone,
+  redlight,
+  speed,
+}
+
+class DummyScreen extends StatefulWidget {
+  @override
+  _DummyScreenState createState() => _DummyScreenState();
+}
+
+class _DummyScreenState extends State<DummyScreen> {
+  Violation _violation = Violation.distance;
+
+  _paddingWidth() {
+    return SizedBox(width: 20.0);
+  }
+
+  // _paddingHeight() {
+  //   return SizedBox(height: 20.0);
+  // }
+
+  _image(name) {
+    return Image.asset(
+      name,
+      width: 184,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,13 +47,104 @@ class DummyScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: Center(
-        child: FAProgressBar(
-          currentValue: 70,
-          progressColor: Colors.blue,
-          backgroundColor: Colors.grey.shade300,
-          direction: Axis.vertical,
-        ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Row>[
+                Row(
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        _image('assets/images/speed.jpg'),
+                        Radio<Violation>(
+                          value: Violation.speed,
+                          groupValue: _violation,
+                          onChanged: (Violation value) {
+                            setState(() => _violation = value);
+                          },
+                        ),
+                      ],
+                    ),
+                    _paddingWidth(),
+                    Column(
+                      children: <Widget>[
+                        _image('assets/images/redlight.jpg'),
+                        Radio<Violation>(
+                          value: Violation.redlight,
+                          groupValue: _violation,
+                          onChanged: (Violation value) {
+                            setState(() => _violation = value);
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        _image('assets/images/distance.jpg'),
+                        Radio<Violation>(
+                          value: Violation.distance,
+                          groupValue: _violation,
+                          onChanged: (Violation value) {
+                            setState(() => _violation = value);
+                          },
+                        ),
+                      ],
+                    ),
+                    _paddingWidth(),
+                    Column(
+                      children: <Widget>[
+                        _image('assets/images/alcohol.jpg'),
+                        Radio<Violation>(
+                          value: Violation.alcohol,
+                          groupValue: _violation,
+                          onChanged: (Violation value) {
+                            setState(() => _violation = value);
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        _image('assets/images/phone.jpg'),
+                        Radio<Violation>(
+                          value: Violation.phone,
+                          groupValue: _violation,
+                          onChanged: (Violation value) {
+                            setState(() => _violation = value);
+                          },
+                        ),
+                      ],
+                    ),
+                    _paddingWidth(),
+                    Column(
+                      children: <Widget>[
+                        _image('assets/images/parking.jpg'),
+                        Radio<Violation>(
+                          value: Violation.parking,
+                          groupValue: _violation,
+                          onChanged: (Violation value) {
+                            setState(() => _violation = value);
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.close),
