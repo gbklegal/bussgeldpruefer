5f6158f4009dab8f7c1c786b95e1398e47e4c0a1
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2021-04-20 17:40:26 +0200

    Add quickCheckViolationName/Index to global

--- app/lib/global.dart
+++ app/lib/global.dart
@@ -1,6 +1,8 @@
 // global file which can contain i.e. global variables
 
 // Quick Check
+String quickCheckViolationName = '';
+int quickCheckViolationIndex;
 String quickCheckTrafficLightColor = 'red';
 String quickCheckLastOfficialLetter = 'Anhörungsbogen';
 
