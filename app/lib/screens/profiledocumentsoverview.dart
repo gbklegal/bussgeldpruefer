e7e945fa0c2b008440da725a4e6de71c06f7209f
ShazeemAshar <45466423+ShazeemAshar@users.noreply.github.com>
2021-05-17 16:17:16 +0200

    Add File Upload

--- app/lib/screens/profiledocumentsoverview.dart
+++ app/lib/screens/profiledocumentsoverview.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
-
 import 'package:app/widgets/appbar.dart';
 import 'package:app/widgets/pagetitle.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/services.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
 
 class ProfileDocumentsOverviewScreen extends StatefulWidget {
   @override
@@ -15,80 +16,92 @@ class ProfileDocumentsOverviewScreen extends StatefulWidget {
 class _ProfileDocumentsOverviewScreenState
     extends State<ProfileDocumentsOverviewScreen> {
   Future<File> imageFile;
+  List<Asset> images = <Asset>[];
+  bool isLoading = false;
 
-  pickImageFromGallery(ImageSource source) {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  uploadImage() async {
     setState(() {
-      imageFile = ImagePicker.pickImage(source: source);
+      isLoading = true;
+    });
+    for (var imageFile in images) {
+      await postImage(imageFile).then((downloadUrl) {
+        // Get the download URL and do your stuff here
+      }).catchError((err) {
+        print(err);
+      });
+    }
+    setState(() {
+      isLoading = false;
     });
   }
 
-  Widget showImage() {
-    return FutureBuilder<File>(
-      future: imageFile,
-      builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
-        if (snapshot.connectionState == ConnectionState.done &&
-            snapshot.data != null) {
-          return Stack(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(top: 13.0, right: 8.0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12.0),
-                  child: Image.file(
-                    snapshot.data,
-                    fit: BoxFit.fill,
-                    width: 100.0,
-                    height: 130.0,
-                  ),
-                ),
-              ),
-              Positioned(
-                right: 0.0,
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.pop(context, true);
-                  },
-                  child: Align(
-                    alignment: Alignment.topRight,
-                    child: CircleAvatar(
-                      radius: 14.0,
-                      backgroundColor: Colors.grey[50],
-                      child: CircleAvatar(
-                          radius: 12.0,
-                          backgroundColor: Colors.blue,
-                          child: Icon(Icons.close, color: Colors.white)),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          );
-          // Card(
-          //   elevation: 5,
-          //   margin: EdgeInsets.all(10),
-          //   shape: RoundedRectangleBorder(
-          //     borderRadius: BorderRadius.circular(16.0),
-          //   ),
-          //   child: Image.file(
-          //     snapshot.data,
-          //     fit: BoxFit.fill,
-          //     width: 100.0,
-          //     height: 100.0,
-          //   ),
-          // );
-        } else if (snapshot.error != null) {
-          return const Text(
-            'Error Picking Image',
-            textAlign: TextAlign.center,
-          );
-        } else {
-          return const Text(
-            'No Image Selected',
-            textAlign: TextAlign.center,
-          );
-        }
-      },
+  Future<dynamic> postImage(Asset imageFile) async {
+    double imageDesiredWidth = 500;
+
+    double getAspectRatio(double originalSize, double desiredSize) =>
+        desiredSize / originalSize;
+    final aspectRatio =
+        getAspectRatio(imageFile.originalWidth.toDouble(), imageDesiredWidth);
+    ByteData byteData = await imageFile.getThumbByteData(
+      (imageFile.originalWidth * aspectRatio).round(),
+      (imageFile.originalHeight * aspectRatio).round(),
+      quality: 100,
     );
+
+    String fileName = DateTime.now().millisecondsSinceEpoch.toString();
+    Reference reference = FirebaseStorage.instance.ref().child(fileName);
+    UploadTask uploadTask = reference.putData(byteData.buffer.asUint8List());
+    TaskSnapshot storageTaskSnapshot =
+        await uploadTask.whenComplete(() => null);
+
+    return await storageTaskSnapshot.ref.getDownloadURL();
+  }
+
+  Future<void> pickImages() async {
+    List<Asset> resultList = <Asset>[];
+
+    try {
+      resultList = await MultiImagePicker.pickImages(
+        maxImages: 5,
+        selectedAssets: images,
+        materialOptions: MaterialOptions(
+          actionBarTitle: "Select Photos",
+        ),
+      );
+    } on Exception catch (e) {
+      print(e);
+    }
+    setState(() {
+      images = resultList;
+    });
+  }
+
+  Widget buildGridView() {
+    if (images.isNotEmpty) {
+      return GridView.count(
+        mainAxisSpacing: 4,
+        crossAxisSpacing: 4,
+        crossAxisCount: 3,
+        children: List.generate(images.length, (index) {
+          Asset asset = images[index];
+          return AssetThumb(
+            asset: asset,
+            width: 100,
+            height: 130,
+          );
+        }),
+      );
+    } else {
+      return const Text(
+        'No Image Selected',
+        textAlign: TextAlign.center,
+      );
+    }
   }
 
   _title(text) {
@@ -178,90 +191,104 @@ class _ProfileDocumentsOverviewScreenState
 
     return Scaffold(
       appBar: AppBarWidget(),
-      body: ListView(
-        children: [
-          PageTitle('Prüfung Bußgeldbescheid'),
-          Padding(
-            padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                _headerTitle(),
-                _padding(),
-                Text(
-                  'Lade Deine Unterlagen hoch',
-                  style: TextStyle(fontSize: 20),
-                ),
-                Text(
-                    'Mit der Kamerafunktion kannst du ganz einfach Fotos von deinen Unterlagen machen oder wähle ein Foto aus deinem Album.'),
-                _padding(),
-                _title('1. Wähle ein Dokument aus, dass du hochladen willst'),
-                SizedBox(
-                  width: double.infinity,
-                  child: DropdownButton<String>(
-                    value: documentName,
-                    icon: const Icon(Icons.expand_more),
-                    iconSize: 24,
-                    elevation: 16,
-                    underline: Container(
-                      height: 2,
-                      color: Colors.blue,
-                    ),
-                    onChanged: (String newValue) {
-                      setState(() => documentName = newValue);
-                    },
-                    items: <String>[
-                      'Anhörungsbogen',
-                      'Bußgeldbescheid',
-                      'Strafbefehl',
-                      'Zeugenanhörungsbogen',
-                      'Verwarnung',
-                    ].map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            PageTitle('Prüfung Bußgeldbescheid'),
+            Padding(
+              padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  _headerTitle(),
+                  _padding(),
+                  Text(
+                    'Lade Deine Unterlagen hoch',
+                    style: TextStyle(fontSize: 20),
                   ),
-                ),
-                _padding(),
-                _title('2. Mache ein Foto von deinem Dokument'),
-                Text('Vergiss nicht die Vorder- und Rückseite'),
-                _padding(),
-                showImage(),
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: IconButton(
-                        icon: Icon(Icons.photo_camera_outlined),
-                        onPressed: () => print('Take photo with camera.'),
+                  Text(
+                      'Mit der Kamerafunktion kannst du ganz einfach Fotos von deinen Unterlagen machen oder wähle ein Foto aus deinem Album.'),
+                  _padding(),
+                  _title('1. Wähle ein Dokument aus, dass du hochladen willst'),
+                  SizedBox(
+                    width: double.infinity,
+                    child: DropdownButton<String>(
+                      value: documentName,
+                      icon: const Icon(Icons.expand_more),
+                      iconSize: 24,
+                      elevation: 16,
+                      underline: Container(
+                        height: 2,
+                        color: Colors.blue,
                       ),
+                      onChanged: (String newValue) {
+                        setState(() => documentName = newValue);
+                      },
+                      items: <String>[
+                        'Anhörungsbogen',
+                        'Bußgeldbescheid',
+                        'Strafbefehl',
+                        'Zeugenanhörungsbogen',
+                        'Verwarnung',
+                      ].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
                     ),
-                    Expanded(
-                      child: IconButton(
-                        icon: Icon(Icons.photo_library_outlined),
-                        onPressed: () =>
-                            pickImageFromGallery(ImageSource.gallery),
-                        // print('Get photo from library.'),
-                      ),
-                    ),
-                  ],
-                ),
-                _padding(),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    child: Text('senden'),
-                    onPressed: () => formFeedback(context),
                   ),
-                ),
-                _padding(),
-                Text(
-                    'Was wir von Dir mindestens  brauchen, um deinen Fall bearbeiten zu können:\n\n- Verwarnungsbogen\n- Bußgeldbescheid und/oder\n- Anhörungsbogen'),
-              ],
+                  _padding(),
+                  _title('2. Mache ein Foto von deinem Dokument'),
+                  Text('Vergiss nicht die Vorder- und Rückseite'),
+                  _padding(),
+                  Container(
+                    constraints: BoxConstraints(
+                      maxHeight: 130,
+                    ),
+                    child: buildGridView(),
+                  ),
+                  _padding(),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: IconButton(
+                          icon: Icon(Icons.photo_camera_outlined),
+                          onPressed: () => print('Take photo with camera.'),
+                        ),
+                      ),
+                      Expanded(
+                        child: IconButton(
+                          icon: Icon(Icons.photo_library_outlined),
+                          onPressed: () => pickImages(),
+                          //pickImageFromGallery(ImageSource.gallery),
+                          // print('Get photo from library.'),
+                        ),
+                      ),
+                    ],
+                  ),
+                  _padding(),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      child: Text('senden'),
+                      onPressed: () {
+                        uploadImage();
+                      },
+                      //formFeedback(context),
+                    ),
+                  ),
+                  isLoading
+                      ? CircularProgressIndicator()
+                      : Visibility(visible: false, child: Text("test")),
+                  _padding(),
+                  Text(
+                      'Was wir von Dir mindestens  brauchen, um deinen Fall bearbeiten zu können:\n\n- Verwarnungsbogen\n- Bußgeldbescheid und/oder\n- Anhörungsbogen'),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
