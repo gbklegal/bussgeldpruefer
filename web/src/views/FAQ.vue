e58a42c9000d71e3ff6d69f0e3367c6455896e76
ilia meshvildishvili <61503627+ilarioni@users.noreply.github.com>
2021-08-04 12:42:24 +0200

    faq
    static input of Q&A from faq.json file

--- web/src/views/FAQ.vue
+++ web/src/views/FAQ.vue
@@ -3,11 +3,37 @@
         <h1 class="text-secondary">FAQ - Häufig gestellt Fragen</h1>
         <Search />
         <div class="mt-7">
+            <FAQItem faqTitle="Welche Kosten kommen auf mich zu?" faqText="<p>Grundsätzlich sind die Leistungen über BussgeldPrüfer kostenlos. Insbesondere können Sie über BussgeldPrüfer grundsätzlich vorerst eine unverbindliche und kostenlose Erstberatung beauftragen und mandatieren. Auf Wunsch beantragt ein hierzu befugter Anwalt für Sie Akteneinsicht in Ihre amtliche Ermittlungsakte. Hierfür berechnet der von Ihnen beauftragte Anwalt lediglich eine pauschale Akteneinsichtsgebühr in Höhe von 13,92 EUR inklusive Mehrwertsteuer. Die Arbeit der Anwälte ist auch in diesem Schritt für Sie garantiert kostenfrei. Anschließend bieten wir Ihnen absolute Kostensicherheit und Transparenz: Wir informieren Sie vorab im Detail über die anstehenden Schritte und die damit verbundenen Kosten des Verfahrens und klären mit Ihnen die Kostenübernahme.</p>" />
+
+            <FAQItem faqTitle="Wird mein Einspruch erfolgreich sein?" faqText="<p>Eine Umfrage hat ergeben, dass mehr als 50 % der in Deutschland verschickten Bußgeldbescheide fehlerhaft sind. Aufgrund dieser hohen Quote, lohnt sich eine Prüfung des Bußgeldbescheids. Damit ein Einspruch auch erfolgreich ist, müssen bestimmte Fristen eingehalten werden.</p>" />
+
+            <FAQItem faqTitle="Wie lange dauert die Prüfung meines Falls?" faqText="<p>Die zuständigen Anwälte und Mitarbeiter sind immer montags bis freitags von 8:00 Uhr – 18 Uhr für Dich da. In der Regel werden alle Fälle in diesem Zeitraum bearbeitet. Innerhalb von 24 Stunden werden Dich die Partneranwälte kontaktieren entweder telefonisch oder per Mail.</p>" />
+
+            <FAQItem faqTitle="Welche Fälle können eingereicht werden?" faqText="<p>Über BussgeldPrüfer können Sie Fälle über die häufigsten Themen zu Bußgeldbescheiden wie: Abstand, Anhörungsbogen, Alkohol, Blitzer, Drogen, Flensburg, Geschwindigkeit, Rote Ampel, Rotlichtverstoß, Parken und Halten, Probezeit sowie MPU und Punkte einreichen. Unsere Partner- und Fachanwälte im Verkehrsrecht beraten und vertreten Sie bei allen Bußgeldverfahren aus dem innerdeutschen Straßenverkehr. Zögern Sie nicht auch Bußgeldfälle im Zusammenhang mit einem Verkehrsunfall über bussgeldprüfer.de einzureichen. Bußgeld schnell und unkompliziert per Einspruch abwehren lassen.</p>" />
+
+            <FAQItem faqTitle="Kann ich gegen einen Bußgeldbescheid vorgehen?" faqText="<p>Solange du die Fristen beachtest und auch einhältst, steht es jedem frei gegen den Bußgeldbescheid vorzugehen. Deshalb lohnt es immer gegen den Bußgeldbescheid vorzugehen.</p>" />
+
+            <FAQItem faqTitle="Benötige ich eine Rechtsschutzversicherung?" faqText="<p>Nein. Für die Dienste von BussgeldPrüfer wird grundsätzlich keine Rechtsschutzversicherung benötigt. Insbesondere bietet BussgeldPrüfer selbst keine Rechtsberatung an. Die Beratung durch einen hierzu befugten Rechtsanwalt kann BussgeldPrüfer nicht ersetzen. Im Rahmen einer späteren Mandatserteilung an unsere Partneranwälte kann jedoch eine Rechtsschutzversicherung sinnvoll sein. Weitere Details hierzu besprechen mit Ihnen die über BussgeldPrüfer beauftragten Anwälte.</p>" />
+
+            <FAQItem faqTitle="Wie lange dauert der gesamte Prozess?" faqText="<p>Jedes Anliegen muss individuell betrachtet werden. Die Partneranwälte werden sich sofort um Dein Anliegen kümmern.  Prozessdauer beträgt zwischen 10 Tagen und mehreren Wochen. Nach Kontaktaufnahme erhältst Du hierzu eine genaue Angabe.</p>" />
+
+            <FAQItem faqTitle="Zu welchem Zeitpunkt soll ich mir Hilfe holen?" faqText="<p>Kontaktiere uns am besten, sobald Du einen Anhörungsbogen oder einen Bußgeldbescheid erhalten hast. Denke daran, dass bestimmte Fristen seitens der Behörde eingehalten werden müssen.</p>" />
+
+            <FAQItem faqTitle="Wie läuft die Beratung ab?" faqText="<p>Nach dem Ausfüllen von „Prüfung Bußgeldbescheid“, wird ein Anwalt der Partnerkanzlei Dich schnellstmöglich kontaktieren. Hier wird aufgezeigt, wie Du gegen dein Bußgeldbescheid vorgehen kannst. Die Erstberatung ist in jedem Fall kostenlos. Nur nach deinem ausdrücklichen Einverständnis wird für dich Einspruch eingelegt. In diesem Zuge wird die Einsicht in Deine Ermittlungsakte beantragt. Darauf folgt ein umfassendes Beratungsgespräch. Danach hast Du die Möglichkeit Dich zu entscheiden, ob Du gegen dein Bußgeldbescheid vorgehen willst, damit ein Mandat erteilt werden kann.</p>" />
+
+            <FAQItem faqTitle="Sind meine Daten sicher?" faqText="<p>Deine Daten sind bei uns sicher und werden ebenso sicher übermittelt. Sicherheit hat für uns die höchste Priorität.</p>" />
+
+            <FAQItem faqTitle="Von wem werde ich vertreten?" faqText="<p>BussgeldPrüfer kooperiert mit renommierten Partneranwälten und – Kanzleien. Diese werden Dich vertreten, wenn du gegen deinen Bußgeldbescheid vorgehen willst. Enger Austausch steht bei uns im Vordergrund.</p>" />
+
+            <FAQItem faqTitle="Was ist der Quick-Check?" faqText="<p>Mit Hilfe des kostenlosen Quick-Checks von BussgeldPrüfer können Sie innerhalb von 5 Minuten eine sofortige erste anwaltliche Ersteinschätzung durch einen Anwalt unserer Partneranwälte erhalten. Danach können Sie entscheiden, ob Sie ein weiteres Vorgehen, z.B. die weitere Einsicht in die amtliche Ermittlungsakte beauftragen möchten. Hierzu können Sie auch ganz einfach unsere App für Apple oder Android benutzen.</p>" />
+
+
+            <!--
             <FAQItem faqTitle="Welche Fälle können über BussgeldPrüfer eingereicht werden?" faqText="<p>Über BussgeldPrüfer können Sie Fälle über die häufigsten Themen zu Bußgeldbescheiden wie: Abstand, Anhörungsbogen, Alkohol, Blitzer, Drogen, Flensburg, Geschwindigkeit, Rote Ampel, Rotlichtverstoß, Parken und Halten, Probezeit sowie MPU und Ounkte einreichen</p><p>Unsere Partner- und Fachanwälte im Verkehrsrecht beraten Sie bei allen Bußgeldverfahren aus dem innerdeutschen Straßenverkehr. Zögern Sie nicht auch Bußgeldfälle im Zusammenhang mit einem Verkehrsunfall über BussgeldPrüfer einzureichen.</p>" />
 
             <FAQItem faqTitle="Welche Fälle können über BussgeldPrüfer eingereicht werden?" faqText="<p>Über BussgeldPrüfer können Sie Fälle über die häufigsten Themen zu Bußgeldbescheiden wie: Abstand, Anhörungsbogen, Alkohol, Blitzer, Drogen, Flensburg, Geschwindigkeit, Rote Ampel, Rotlichtverstoß, Parken und Halten, Probezeit sowie MPU und Ounkte einreichen</p><p>Unsere Partner- und Fachanwälte im Verkehrsrecht beraten Sie bei allen Bußgeldverfahren aus dem innerdeutschen Straßenverkehr. Zögern Sie nicht auch Bußgeldfälle im Zusammenhang mit einem Verkehrsunfall über BussgeldPrüfer einzureichen.</p>" />
+            -->
 
-            <FAQItem faqTitle="Welche Fälle können über BussgeldPrüfer eingereicht werden?" faqText="<p>Über BussgeldPrüfer können Sie Fälle über die häufigsten Themen zu Bußgeldbescheiden wie: Abstand, Anhörungsbogen, Alkohol, Blitzer, Drogen, Flensburg, Geschwindigkeit, Rote Ampel, Rotlichtverstoß, Parken und Halten, Probezeit sowie MPU und Ounkte einreichen</p><p>Unsere Partner- und Fachanwälte im Verkehrsrecht beraten Sie bei allen Bußgeldverfahren aus dem innerdeutschen Straßenverkehr. Zögern Sie nicht auch Bußgeldfälle im Zusammenhang mit einem Verkehrsunfall über BussgeldPrüfer einzureichen.</p>" />
         </div>
     </div>
 </template>
