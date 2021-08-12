699b37f2b6e0d90faed58b989495448382187130
GBK-IT01 <gbk-it01@GBK-IT01.fritz.box>
2021-08-12 17:00:41 +0200

    datenschutz 2 punkt

--- web/src/views/Privacy.vue
+++ web/src/views/Privacy.vue
@@ -1,12 +1,29 @@
 <template>
     <div class="container">
         <h1>Datenschutzerklärung</h1>
-        <h2 class="text-lg font-bold">Name und Kontaktdaten des für die Verarbeitung Verantwortlichen</h2>
-        <div>
-            <p>Diese Datenschutzhinweise gelten für die Datenverarbeitung durch:</p>
-            <p>Verantwortlicher:</p>
-            <p>GDB L-Tech Unternehmergesellschaft (haftungsbeschränkt)<br>(im Folgenden ggf. auch: „GDB“ oder „wir“),<br>vertreten durch den Geschäftsführer Herrn Tobias Gußmann,<br>
-            Nordostpark 102A, 90411 Nürnberg, Deutschland<br>Sitz: Nürnberg<br>Handelsregister: Amtsgericht Nürnberg HRB 33696<br>E-Mail: <a href="mailto:dialog@bussgeldpruefer.de">dialog@bussgeldpruefer.de</a><br>Telefon: <a href="tel:091112345678">0911/12345678</a></p>
-        </div> 
+        <ol class="ml-5 list-decimal">
+            <li>
+                <h2 class="text-lg font-bold">Name und Kontaktdaten des für die Verarbeitung Verantwortlichen</h2>
+                <div class="mb-4">
+                    <p>Diese Datenschutzhinweise gelten für die Datenverarbeitung durch:</p>
+                    <p>Verantwortlicher:</p>
+                    <p>GDF GmbH (im Folgenden ggf. auch: „GDF“ oder „wir“),<br>vertreten durch den Geschäftsführer Herrn Tobias Gußmann,<br>Kaiserstraße 23, 90403 Nürnberg, Deutschland<br>
+                    Sitz: Nürnberg<br>Handelsregister: Amtsgericht Nürnberg HRB 33156<br>E-Mail: <a href="mailto:dialog@bussgeldpruefer.com">dialog@bussgeldpruefer.com</a><br>Telefon: <a href="tel:0911893103444">0911/893103-444</a></p>
+                </div>
+            </li>
+            <li>
+                <h2 class="text-lg font-bold">Erhebung und Speicherung personenbezogener Daten sowie Art und Zweck von deren Verwendung</h2>
+                <ol>
+                    <li class="ml-8 list-disc">
+                        <p>Beim Besuch der Website</p>
+                    </li>
+                </ol>
+                <div>
+                    <p class="mb-2">Beim Aufrufen unserer Website www.steuermachen.de werden durch den auf Ihrem Endgerät zum Einsatz kommenden Browser automatisch Informationen an den Server unserer Website gesendet. Diese Informationen werden temporär in einem sog. Logfile gespeichert. Folgende Informationen werden dabei ohne Ihr Zutun erfasst und bis zur automatisierten Löschung gespeichert:</p>
+                    <p class="ml-4 mb-4">IP-Adresse des anfragenden Rechners,<br>Datum und Uhrzeit des Zugriffs,<br>Name und URL der abgerufenen Datei,<br>Website, von der aus der Zugriff erfolgt (Referrer-URL),<br>verwendeter Browser und ggf. das Betriebssystem Ihres Rechners sowie der Name Ihres Access-Providers.</p>
+                    <p>Die genannten Daten werden durch uns zu folgenden Zwecken verarbeitet:</p>
+                </div>
+            </li>
+        </ol>
     </div>
 </template>
