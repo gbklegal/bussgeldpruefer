d61fccd2e757cd61c109d307c6cd7dbe9edc883d
GBK-IT01 <gbk-it01@GBK-IT01.fritz.box>
2021-08-12 16:25:46 +0200

    datenschutz vue file

--- web/src/views/Privacy.vue
+++ web/src/views/Privacy.vue
@@ -1,16 +1,12 @@
 <template>
     <div class="container">
         <h1>Datenschutzerklärung</h1>
-        <ol class="ml-5 list-decimal">
-            <li>
-                <h2 class="text-lg font-bold">Name und Kontaktdaten des für die Verarbeitung Verantwortlichen</h2>
-                <div>
-                    <p>Diese Datenschutzhinweise gelten für die Datenverarbeitung durch:</p>
-                    <p>Verantwortlicher:</p>
-                    <p>GDB L-Tech Unternehmergesellschaft (haftungsbeschränkt)<br>(im Folgenden ggf. auch: „GDB“ oder „wir“),<br>vertreten durch den Geschäftsführer Herrn Tobias Gußmann,<br>
-                    Nordostpark 102A, 90411 Nürnberg, Deutschland<br>Sitz: Nürnberg<br>Handelsregister: Amtsgericht Nürnberg HRB 33696<br>E-Mail: <a href="mailto:dialog@bussgeldpruefer.de">dialog@bussgeldpruefer.de</a><br>Telefon: <a href="tel:091112345678">0911/12345678</a></p>
-                </div>
-            </li>
-        </ol>
+        <h2 class="text-lg font-bold">Name und Kontaktdaten des für die Verarbeitung Verantwortlichen</h2>
+        <div>
+            <p>Diese Datenschutzhinweise gelten für die Datenverarbeitung durch:</p>
+            <p>Verantwortlicher:</p>
+            <p>GDB L-Tech Unternehmergesellschaft (haftungsbeschränkt)<br>(im Folgenden ggf. auch: „GDB“ oder „wir“),<br>vertreten durch den Geschäftsführer Herrn Tobias Gußmann,<br>
+            Nordostpark 102A, 90411 Nürnberg, Deutschland<br>Sitz: Nürnberg<br>Handelsregister: Amtsgericht Nürnberg HRB 33696<br>E-Mail: <a href="mailto:dialog@bussgeldpruefer.de">dialog@bussgeldpruefer.de</a><br>Telefon: <a href="tel:091112345678">0911/12345678</a></p>
+        </div> 
     </div>
 </template>
