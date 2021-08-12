12f8e06edac4688931a5c44c1d11fe5758e480a2
ShazeemAshar <45466423+ShazeemAshar@users.noreply.github.com>
2021-08-12 11:46:55 +0200

    Update WordPress API

--- app/lib/services/http_service.dart
+++ app/lib/services/http_service.dart
@@ -4,12 +4,10 @@ import 'package:app/models/post.dart';
 import 'package:http/http.dart';
 
 class HttpService {
-  final String postsUrl =
-      "https://xn--bussgeldprfer-5ob.com/wp-json/wp/v2/posts?_embed&_fields=id,date,link,title,content,_links,_embedded";
   // ignore: missing_return
   Future<List<Post>> getPosts(pageNumber) async {
     Response response = await get(Uri.parse(
-        "https://xn--bussgeldprfer-5ob.com/wp-json/wp/v2/posts?page=$pageNumber&_embed&_fields=id,date,link,title,content,_links,_embedded"));
+        "https://xn--bussgeldprfer-5ob.com/bgp/wp-api.php/posts?page=$pageNumber&_embed&_fields=id,date,link,title,content,_links,_embedded"));
     try {
       if (response.statusCode == 200) {
         Iterable body = jsonDecode(response.body);
