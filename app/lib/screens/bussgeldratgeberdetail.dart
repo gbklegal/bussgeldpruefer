7d8c0efa6739f4e489c50ffd8ffb37c14a8bc465
ShazeemAshar <45466423+ShazeemAshar@users.noreply.github.com>
2021-08-12 11:35:00 +0200

    Update Bußgeld Ratgeber

--- app/lib/screens/bussgeldratgeberdetail.dart
+++ app/lib/screens/bussgeldratgeberdetail.dart
@@ -7,7 +7,7 @@ import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart
 
 class BussgeldRatgeberDetailScreen extends StatelessWidget {
   final Post post;
-  final f = new DateFormat('yyyy-MM-dd hh:mm');
+  final f = new DateFormat('dd-MM-yyyy hh:mm');
 
   BussgeldRatgeberDetailScreen(this.post);
 
