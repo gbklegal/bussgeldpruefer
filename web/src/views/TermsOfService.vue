fa129e2f93bb9fffc7329026b7aea2b31b0538ea
ilarioni mesh <ilia.meshvildishvili@gmail.com>
2021-08-19 15:25:03 +0200

    public

--- web/src/views/TermsOfService.vue
+++ web/src/views/TermsOfService.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
         <h1>Rechtliche Hinweise</h1>
-        <p>Hier finden Sie rechtliche Hinweise zur Nutzung unserer Webseiten -  <u><a href="../assets/img/Rechtliche-Hinweise_NoCopy.pdf" target="_blank">Hier klicken</a></u> </p>
+        <p>Hier finden Sie rechtliche Hinweise zur Nutzung unserer Webseiten -  <u><a href="./rh.pdf" target="_blank">Hier klicken</a></u> </p>
     </div>
 </template>
