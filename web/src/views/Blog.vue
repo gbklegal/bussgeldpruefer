731d7b412a15b77b4ff6968dc46c45694c659da4
ilarioni mesh <ilia.meshvildishvili@gmail.com>
2021-08-26 15:34:48 +0200

    fetching data test

--- web/src/views/Blog.vue
+++ web/src/views/Blog.vue
@@ -18,6 +18,15 @@
             <router-link :to="{ name: 'blog-single' }">
                 <BlogPreview imgsrc="/src/assets/img/alcohol.jpg" imgalt="Bild mit Alkohol" blogtitle="Führerscheinverlust bei Alkoholkonsum" previewtext="Die Tempo-30er-Zone erfüllt drei grundlegende Funktionen. So soll die 30-Zone die Verkehrssicherheit erhöhen. Aufgrund eines Verkehrssicherheitsprogrammes führte Berlin vor fast allen Schulen und Kitas die 30er Zone ein." />
             </router-link>
+
+            <!-- test -->
+            <div v-for="bus in buss" :key="bus.id">
+                <h2> Date - {{ bus.date }} </h2>
+                <h2> Title - {{ bus.title.rendered }} </h2>
+                <p> {{ bus.content.rendered }} </p>
+                <p> {{ bus._embedded["wp:featuredmedia"][0].media_details.file }} </p>
+                <p> {{ bus._embedded["wp:featuredmedia"][0].media_details.sizes.thumbnail.source_url }} </p>
+            </div>
         </div>
 
         <button class="btn-primary mt-10 mx-auto block">Mehr laden</button>
@@ -31,6 +40,18 @@ export default {
     components: {
         BlogPreview,
         BlogSearch
+    },
+    data() {
+        return {
+            buss: []
+        }
+    },
+
+    created() {
+    fetch('https://xn--bussgeldprfer-5ob.com/wp-json/wp/v2/posts?_embed&_fields=id,date,link,title,content,_links,_embedded')
+      .then((res) => res.json())
+      .then(data => this.buss = data)
+      .catch(err => console.log(err.message))
     }
 }
 </script>
