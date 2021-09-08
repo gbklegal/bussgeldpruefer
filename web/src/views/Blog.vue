2edf53da829b2782962c4241b5979fbcfd52dcfc
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2021-09-08 17:29:26 +0200

    Update Blog.vue
    new API URL, status fallback and loading circle

--- web/src/views/Blog.vue
+++ web/src/views/Blog.vue
@@ -13,26 +13,36 @@
       </router-link>
     </div>
 
-    <button class="btn-primary mt-10 mx-auto block">Mehr laden</button>
+    <div class="mt-10 mx-auto w-8" v-if="!hideLoadingCircle">
+      <LoadingCircle />
+    </div>
+    <button v-if="!hideLoadMoreButton" class="btn-primary mt-10 mx-auto block" @click="loadMorePosts">Mehr laden</button>
   </div>
 </template>
 
 
 <script>
+import global from '../global.js';
 import BlogPreview from "../components/BlogPreview.vue";
 import BlogSearch from "../components/BasicSearchForm.vue";
+import LoadingCircle from "../components/BasicLoadingCircle.vue";
 
 export default {
   components: {
     BlogPreview,
     BlogSearch,
+    LoadingCircle
   },
 
   data() {
     return {
+      apiURL: global.api.urlBuilder(),
       blogPosts: [],
       searchQuery: this.$route.query.search || '',
-      postCount: 0
+      postCount: 0,
+      currentPage: 1,
+      hideLoadMoreButton: false,
+      hideLoadingCircle: true
     };
   },
 
@@ -50,47 +60,86 @@ export default {
       return htmlString.replace(/<[^>]*>/gim, '');
     },
     fetchPostData: function() {
-      let apiURL = 'https://xn--bussgeldprfer-5ob.com/wp-json/wp/v2/posts?_embed&_fields=id,date,modified,slug,link,title,content,_links,_embedded';
+      // display loading circle
+      this.hideLoadingCircle = false;
+
+      let apiURL = this.apiURL + '&page=' + this.currentPage
       if (this.searchQuery) {
         apiURL += '&search=' + this.searchQuery;
       }
 
-      console.log(apiURL);
+      // console.log(apiURL);
 
-      fetch(apiURL)
-        .then(resp => resp.json())
-        .then(postsData => {
-          // console.log(postsData);
-          this.blogPosts = [];
-          this.postCount = postsData.length;
+      let xhr = new XMLHttpRequest();
+      let formData = new FormData();
 
-          postsData.forEach(postData => {
-              // console.log(postData);
-              let filteredData = {
-                  key: this.getRandomInt(100, 999),
-                  id: postData.id,
-                  date: postData.modified, // date
-                  slug: postData.slug,
-                  title: postData.title.rendered,
-                  content: postData.content.rendered,
-                  contentTeaser: this.removeHtmlTags(postData.content.rendered).substring(0, 250) + '...',
-                  images: {
-                      full: postData._embedded["wp:featuredmedia"][0].source_url,
-                      medium: postData._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url,
-                      thumbnail: postData._embedded["wp:featuredmedia"][0].media_details.sizes.thumbnail.source_url,
-                      openpress: {
-                          blogSmall: postData._embedded["wp:featuredmedia"][0].media_details.sizes['onepress-blog-small'].source_url,
-                          small: postData._embedded["wp:featuredmedia"][0].media_details.sizes['onepress-small'].source_url,
-                          medium: postData._embedded["wp:featuredmedia"][0].media_details.sizes['onepress-medium'].source_url
-                      }
-                  },
-                  imageCaption: postData._embedded["wp:featuredmedia"][0].caption.rendered
-              };
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            let postsData = JSON.parse(xhr.responseText);
 
-              this.blogPosts.push(filteredData)
-          });
-        })
-        .catch((err) => console.error(err));
+            // console.log(postsData);
+            // this.blogPosts = [];
+            this.postCount = postsData.length;
+
+            postsData.forEach(postData => {
+                // console.log(postData);
+                let filteredData = {
+                    key: this.getRandomInt(100, 999),
+                    id: postData.id,
+                    date: postData.modified, // date
+                    slug: postData.slug,
+                    title: postData.title.rendered,
+                    content: postData.content.rendered,
+                    contentTeaser: this.removeHtmlTags(postData.content.rendered).substring(0, 250) + '...',
+                    images: {
+                        full: postData._embedded["wp:featuredmedia"][0].source_url,
+                        medium: postData._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url,
+                        thumbnail: postData._embedded["wp:featuredmedia"][0].media_details.sizes.thumbnail.source_url,
+                        openpress: {
+                            blogSmall: postData._embedded["wp:featuredmedia"][0].media_details.sizes['onepress-blog-small'].source_url,
+                            small: postData._embedded["wp:featuredmedia"][0].media_details.sizes['onepress-small'].source_url,
+                            medium: postData._embedded["wp:featuredmedia"][0].media_details.sizes['onepress-medium'].source_url
+                        }
+                    },
+                    imageCaption: postData._embedded["wp:featuredmedia"][0].caption.rendered
+                };
+
+                this.blogPosts.push(filteredData)
+
+                // hide loading circle
+                this.hideLoadingCircle = true;
+            });
+          } else {
+            this.hideLoadMoreButton = true;
+          }
+        }
+      };
+      xhr.open('GET', apiURL, true);
+      xhr.send(formData);
+
+      // fetch(apiURL)
+      //   .then(resp => resp.json())
+      //   .then(postsData => {
+          
+      //   })
+      //   .catch((err) => console.error(err));
+    },
+    checkNextPage: function(callback) {
+      let nextPageNumber = this.currentPage + 1;
+      fetch(this.apiURL + '&page=' + nextPageNumber).then(resp => {
+        if (callback) callback(resp.ok);
+      });
+    },
+    loadMorePosts: function() {
+      this.hideLoadingCircle = false;
+      this.currentPage += 1;
+      this.checkNextPage(respOK => {
+        if (!respOK) this.hideLoadMoreButton = true;
+
+        this.fetchPostData();
+        console.log('loadMorePosts');
+      });
     }
   },
 
