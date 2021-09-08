96b19e5741834c7a0e6d9bbc95249d08424c3652
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2021-09-08 17:26:13 +0200

    Create BasicSkeleton.vue
    add skeleton component

---
+++ web/src/components/BasicSkeleton.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="skeleton">
+        <div class="skeleton-box w-full h-96"></div>
+        <div class="skeleton-box w-50 h-10 mt-5"></div>
+        <div class="skeleton-box w-1/3 h-6 mt-6"></div>
+        <div class="skeleton-box w-1/4 h-6 mt-1"></div>
+        <hr class="my-4">
+        <div class="skeleton-box w-full h-10 mt-5"></div>
+        <div class="skeleton-box w-full h-40 mt-5"></div>
+        <div class="skeleton-box w-full h-40 mt-5"></div>
+    </div>
+</template>
+
+
+<style>
+.skeleton {
+    position: relative;
+    overflow: hidden;
+}
+
+.skeleton::after {
+    top: 0;
+    position: absolute;
+    content: '';
+    background-image: linear-gradient( 100deg, transparent, rgba(255, 255, 255, 0.5) 50%, transparent 80% );
+    background-repeat: repeat-y;
+    left: -100%;
+    width: 100px;
+    height: 100%;
+    animation: 1s shine infinite;
+}
+
+.skeleton-box {
+    background-color: lightgrey;
+}
+
+@keyframes shine {
+    to {
+        left: 100%;
+    }
+}
+</style>
+
+
+<script>
+export default {
+    name: 'Skeleton',
+    props: {
+        type: String
+    }
+}
+</script>
