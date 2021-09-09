9fea776dcfe07eb764eeca47365ced2577521118
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2021-09-09 16:20:51 +0200

    Update Home.vue
    replaced placeholder with real content and add skeleton

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -62,9 +62,20 @@
         <h2 class="text-secondary font-normal mb-3">Bußgeld Ratgeber</h2>
         <p class="text-secondary mb-10">Aktuelle Neuigkeiten, Änderungen &amp; Urteile zum Thema Bußgeld</p>
 
-        <BlogPreview imgsrc="/src/assets/img/alcohol.jpg" imgalt="Bild mit Alkohol" blogtitle="Führerscheinverlust bei Alkoholkonsum" previewtext="Die Tempo-30er-Zone erfüllt drei grundlegende Funktionen. So soll die 30-Zone die Verkehrssicherheit erhöhen. Aufgrund eines Verkehrssicherheitsprogrammes führte Berlin vor fast allen Schulen und Kitas die 30er Zone ein."/>
+        <div v-if="!hideSkeleton">
+          <Skeleton v-for="skeleton in postCount" :key="skeleton" type="blogPreview" />
+        </div>
 
-        <BlogPreview imgsrc="/src/assets/img/alcohol.jpg" imgalt="Bild mit Alkohol" blogtitle="Führerscheinverlust bei Alkoholkonsum" previewtext="Die Tempo-30er-Zone erfüllt drei grundlegende Funktionen. So soll die 30-Zone die Verkehrssicherheit erhöhen. Aufgrund eines Verkehrssicherheitsprogrammes führte Berlin vor fast allen Schulen und Kitas die 30er Zone ein."/>
+        <div v-for="blogPost in blogPosts" :key="blogPost.id">
+          <router-link :to="{ name: 'blog-single', params: { postSlug: blogPost.slug } }">
+            <BlogPreview
+              :blogtitle="blogPost.title"
+              :previewtext="blogPost.contentPreview"
+              :imgsrc="blogPost.image"
+              :imgalt="blogPost.imageCaption"
+            />
+          </router-link>
+        </div>
 
         <router-link class="btn-primary text-white font-normal inline-block mt-10" to="/ratgeber">
           mehr erfahren
@@ -85,7 +96,7 @@
           <FeedbackText quote="Ich war anfangs total überfordert, aber das BussgeldPruefer hat mir den gesamten Prozess abgenommen." author="Melis Y." />
           <FeedbackText quote="Ich war anfangs total überfordert, aber das BussgeldPruefer hat mir den gesamten Prozess abgenommen." author="Melis Y." />
           <FeedbackText quote="Ich war anfangs total überfordert, aber das BussgeldPruefer hat mir den gesamten Prozess abgenommen." author="Melis Y." />
-        </div>        
+        </div>
       </div>
     </section>
 
@@ -93,6 +104,7 @@
 </template>
 
 <script>
+import global from '../global';
 import CTA from '../components/BasicCTA.vue'
 import Steps from '../components/BasicSteps.vue'
 import BlogPreview from '../components/BlogPreview.vue'
@@ -100,6 +112,7 @@ import TextOne from '../components/BasicSuccessOne.vue'
 import Success from '../components/BasicSuccessTwo.vue'
 import FeedbackStars from '../components/FeedbackStars.vue'
 import FeedbackText from '../components/FeedbackText.vue'
+import Skeleton from '../components/BasicSkeleton.vue'
 
 export default {
   name: 'Home',
@@ -110,8 +123,47 @@ export default {
     TextOne,
     Success,
     FeedbackStars,
-    FeedbackText
+    FeedbackText,
+    Skeleton
   },
+
+  data() {
+    return {
+      apiURL: global.api.urlBuilder(),
+      postCount: 2,
+      blogPosts: [],
+      hideSkeleton: false
+    }
+  },
+
+  methods: {
+    fetchPostData: function() {
+      let apiURL = this.apiURL + '&per_page=' + this.postCount;
+
+      fetch(apiURL).then(resp => resp.json()).then(postsData => {
+        postsData.forEach(postData => {
+          let filteredData = {
+            id: postData.id,
+            slug: postData.slug,
+            title: postData.title.rendered,
+            content: postData.content.rendered,
+            contentPreview: global.removeHtmlTags(postData.content.rendered).substring(0, 250) + '...',
+            image: postData._embedded["wp:featuredmedia"][0].media_details.sizes['onepress-medium'].source_url,
+            imageCaption: postData._embedded["wp:featuredmedia"][0].caption.rendered
+          };
+          this.blogPosts.push(filteredData);
+        });
+
+        // hide skeleton
+        this.hideSkeleton = true;
+      });
+    }
+  },
+
+  created() {
+    console.log(this.apiURL);
+    this.fetchPostData();
+  }
 };
 </script>
 
