07a41b15018faff3d6571a5326f389219b3fd4f1
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2021-09-09 16:24:39 +0200

    add load more

--- web/src/views/BlogAuthor.vue
+++ web/src/views/BlogAuthor.vue
@@ -13,72 +13,174 @@
         </router-link>
         </div>
 
-        <button class="btn-primary mt-10 mx-auto block">Mehr laden</button>
+        <div class="mt-10 mx-auto w-8" v-if="!hideLoadingCircle">
+          <LoadingCircle />
+        </div>
+        <button v-if="!hideLoadMoreButton" class="btn-primary mt-10 mx-auto block" @click="loadMorePosts">Mehr laden</button>
     </div>
 </template>
 
 
 <script>
+import global from '../global';
 import BlogPreview from "../components/BlogPreview.vue";
 import BlogSearch from "../components/BasicSearchForm.vue";
+import LoadingCircle from "../components/BasicLoadingCircle.vue";
 
 export default {
   components: {
     BlogPreview,
     BlogSearch,
+    LoadingCircle
   },
 
   data() {
     return {
+      apiURL: global.api.urlBuilder(),
       blogPosts: [],
-      authorName: ''
+      authorName: '',
+      authorId: null,
+      currentPage: 1,
+      hideLoadMoreButton: false,
+      hideLoadingCircle: true,
     };
   },
 
   methods: {
+    getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min) ) + min;
+    },
     removeHtmlTags: htmlString => {
       return htmlString.replace(/<[^>]*>/gim, '');
+    },
+    getAuthorIdBySlug: function(authorSlug, callback) {
+      let apiURL = global.api.base + '?author_slug=' + authorSlug;
+      fetch(apiURL).then(resp => resp.json()).then(data => {
+        if (callback) callback(data.user.id);
+      });
+    },
+    fetchPostData: function() {
+      // display loading circle
+      this.hideLoadingCircle = false;
+
+      let apiURL = `${this.apiURL}&page=${this.currentPage}&author=${this.authorId}`;
+      // if (this.searchQuery) {
+      //   apiURL += '&search=' + this.searchQuery;
+      // }
+
+      let xhr = new XMLHttpRequest();
+      let formData = new FormData();
+
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            let postsData = JSON.parse(xhr.responseText);
+            this.authorName = postsData[0]._embedded.author[0].name;
+            this.postCount = postsData.length;
+
+            postsData.forEach(postData => {
+                let filteredData = {
+                    key: this.getRandomInt(100, 999),
+                    id: postData.id,
+                    date: postData.modified, // date
+                    slug: postData.slug,
+                    title: postData.title.rendered,
+                    content: postData.content.rendered,
+                    contentPreview: this.removeHtmlTags(postData.content.rendered).substring(0, 250) + '...',
+                    images: {
+                        full: postData._embedded["wp:featuredmedia"][0].source_url,
+                        medium: postData._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url,
+                        thumbnail: postData._embedded["wp:featuredmedia"][0].media_details.sizes.thumbnail.source_url,
+                        onepress: {
+                            blogSmall: postData._embedded["wp:featuredmedia"][0].media_details.sizes['onepress-blog-small'].source_url,
+                            small: postData._embedded["wp:featuredmedia"][0].media_details.sizes['onepress-small'].source_url,
+                            medium: postData._embedded["wp:featuredmedia"][0].media_details.sizes['onepress-medium'].source_url
+                        }
+                    },
+                    imageCaption: postData._embedded["wp:featuredmedia"][0].caption.rendered
+                };
+
+                this.blogPosts.push(filteredData)
+
+                // hide loading circle
+                this.hideLoadingCircle = true;
+            });
+          } else {
+            this.hideLoadMoreButton = true;
+          }
+        }
+      };
+      xhr.open('GET', apiURL, true);
+      xhr.send(formData);
+
+      // fetch(apiURL)
+      //   .then(resp => resp.json())
+      //   .then(postsData => {
+          
+      //   })
+      //   .catch((err) => console.error(err));
+    },
+    checkNextPage: function(callback) {
+      let nextPageNumber = this.currentPage + 1;
+      fetch(this.apiURL + '&page=' + nextPageNumber).then(resp => {
+        if (callback) callback(resp.ok);
+      });
+    },
+    loadMorePosts: function() {
+      this.hideLoadingCircle = false;
+      this.currentPage += 1;
+      this.checkNextPage(respOK => {
+        console.log(respOK);
+        if (!respOK) this.hideLoadMoreButton = true;
+
+        this.fetchPostData();
+        console.log('loadMorePosts');
+      });
     }
+    // fetchPostsDataByAuthorId: function(authorId) {
+    //   fetch(this.apiURL + '&author=' + authorId)
+    //     .then((res) => res.json())
+    //     .then((postsData) => {
+    //       // console.log(postsData);
+    //       this.blogPosts = [];
+
+    //       this.authorName = postsData[0]._embedded.author[0].name;
+    //       console.log(this.authorName, this.$route.params.authorSlug, this.apiURL + '&author=' + this.$route.params.authorSlug);
+
+    //       postsData.forEach(postData => {
+    //           // console.log(postData);
+    //           let filteredData = {
+    //               id: postData.id,
+    //               date: postData.modified, // date
+    //               slug: postData.slug,
+    //               title: postData.title.rendered,
+    //               content: postData.content.rendered,
+    //               contentPreview: this.removeHtmlTags(postData.content.rendered).substring(0, 250) + '...',
+    //               images: {
+    //                   full: postData._embedded['wp:featuredmedia'][0].source_url,
+    //                   medium: postData._embedded['wp:featuredmedia'][0].media_details.sizes.medium.source_url,
+    //                   thumbnail: postData._embedded['wp:featuredmedia'][0].media_details.sizes.thumbnail.source_url,
+    //                   onepress: {
+    //                       blogSmall: postData._embedded['wp:featuredmedia'][0].media_details.sizes['onepress-blog-small'].source_url,
+    //                       small: postData._embedded['wp:featuredmedia'][0].media_details.sizes['onepress-small'].source_url,
+    //                       medium: postData._embedded['wp:featuredmedia'][0].media_details.sizes['onepress-medium'].source_url
+    //                   }
+    //               },
+    //               imageCaption: postData._embedded['wp:featuredmedia'][0].caption.rendered
+    //           };
+
+    //           this.blogPosts.push(filteredData)
+    //       });
+    //     })
+    //     .catch((err) => console.error(err.message));
+    // }
   },
 
   beforeMount() {
-    fetch(
-      'https://xn--bussgeldprfer-5ob.com/wp-json/wp/v2/posts?_embed&_fields=id,date,modified,slug,link,title,content,_links,_embedded&author-slug=' + this.$route.params.authorSlug
-    )
-      .then((res) => res.json())
-      .then((postsData) => {
-        // console.log(postsData);
-        this.blogPosts = [];
-
-        this.authorName = postsData[0]._embedded.author[0].name;
-
-        postsData.forEach(postData => {
-            // console.log(postData);
-            let filteredData = {
-                id: postData.id,
-                date: postData.modified, // date
-                slug: postData.slug,
-                title: postData.title.rendered,
-                content: postData.content.rendered,
-                contentTeaser: this.removeHtmlTags(postData.content.rendered).substring(0, 250) + '...',
-                images: {
-                    full: postData._embedded['wp:featuredmedia'][0].source_url,
-                    medium: postData._embedded['wp:featuredmedia'][0].media_details.sizes.medium.source_url,
-                    thumbnail: postData._embedded['wp:featuredmedia'][0].media_details.sizes.thumbnail.source_url,
-                    openpress: {
-                        blogSmall: postData._embedded['wp:featuredmedia'][0].media_details.sizes['onepress-blog-small'].source_url,
-                        small: postData._embedded['wp:featuredmedia'][0].media_details.sizes['onepress-small'].source_url,
-                        medium: postData._embedded['wp:featuredmedia'][0].media_details.sizes['onepress-medium'].source_url
-                    }
-                },
-                imageCaption: postData._embedded['wp:featuredmedia'][0].caption.rendered
-            };
-
-            this.blogPosts.push(filteredData)
-        });
-      })
-      .catch((err) => console.error(err.message));
-
-  },
+    this.getAuthorIdBySlug(this.$route.params.authorSlug, authorId => {
+      this.authorId = authorId;
+      this.fetchPostData();
+    });
+  }
 };
 </script>
