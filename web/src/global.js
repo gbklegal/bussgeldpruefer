8322dcd59dda8befe9a0b44c1201a61bc2fba960
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2021-09-09 16:20:09 +0200

    add removeHtmlTags

--- web/src/global.js
+++ web/src/global.js
@@ -1,14 +1,17 @@
 export default {
     api: {
-        // base: 'https://xn--bussgeldprfer-5ob.com',
+        domain: 'https://xn--bussgeldprfer-5ob.com',
         base: 'https://xn--bussgeldprfer-5ob.com/bgp/wp-api.php',
         bgp: 'https://xn--bussgeldprfer-5ob.com/bgp',
         wp: '/wp-json/wp/v2/',
-        type: 'posts',
+        type: '/posts',
         filter: '?_embed&_fields=id,date,modified,slug,link,title,content,_links,_embedded',
         urlBuilder: function(type, withFilter) {
-            return this.base + this.wp + this.type + this.filter
+            return this.base + this.type + this.filter
             // return this.base + this.wp + type + (withFilter ? this.filter : '');
         }
+    },
+    removeHtmlTags: htmlString => {
+        return htmlString.replace(/<[^>]*>/gim, '');
     }
 }
