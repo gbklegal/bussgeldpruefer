ecb682dc51c85bb596772ddabce02c577c9a8138
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2021-09-24 17:01:42 +0200

    Update NotFound.vue
    center to content and add new search parameter

--- web/src/views/NotFound.vue
+++ web/src/views/NotFound.vue
@@ -1,6 +1,8 @@
 <template>
-    <h2>Seite nicht gefunden</h2>
-    <BlogSearch />
+    <div class="flex flex-col items-center py-16">
+        <h2>Seite nicht gefunden</h2>
+        <BlogSearch url="/ratgeber/" :noHeader="true" />
+    </div>
 </template>
 
 
