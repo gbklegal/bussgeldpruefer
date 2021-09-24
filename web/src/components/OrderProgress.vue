a999d722d5457c6ac4c0b6f4ca925cc3c1d6ff2f
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2021-09-24 17:06:39 +0200

    Update progress bar
    add animation

--- web/src/components/OrderProgress.vue
+++ web/src/components/OrderProgress.vue
@@ -1,6 +1,9 @@
 <template>
-    <label class="text-xs float-right" for="order-progress">{{ text }}</label>
     <progress id="order-progress" :value="percentage" max="100" class="bg-gray-200 block w-full rounded">{{ percentage }}%</progress>
+    <!-- <progress id="order-progress" :value="percentage" max="100" class="bg-gray-200 block w-full rounded">{{ percentage }}%</progress> -->
+    <div class="progress-wrapper bg-gray-200 block w-full rounded">
+        <div class="progress bg-gray-200 block rounded" :style="'width:'+percentage+'%'" id="progressBar">&nbsp;</div>
+    </div>
 </template>
 <script>
 export default {
@@ -12,28 +15,37 @@ export default {
 </script>
 <style scoped>
 /* Reset default */
-progress[value] {
+progress[value],
+.progress-wrapper {
     -webkit-appearance: none;
        -moz-appearance: none;
             appearance: none;
-    border: none; 
+    border: none;
     color: #5CC8C5;
 }
 
-progress[value]::-webkit-progress-bar {
-    /*background-color: #eee;*/
-    background-color: #fff;
-    border-radius: 3px;
+.progress-wrapper,
+.progress {
     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.25) inset;
 }
 
-progress[value]::-moz-progress-bar {
+progress[value]::-webkit-progress-bar,
+.progress {
+    /*background-color: #eee;*/
+    background-color: #fff;
+    border-radius: 3px;
+}
+
+progress[value]::-moz-progress-bar,
+.progress {
     background-color: #5CC8C5;
     border-radius: 3px;
 }
 
-progress[value]::-webkit-progress-value {
+progress[value]::-webkit-progress-value,
+.progress {
     background-color: #5CC8C5;
     border-radius: 3px;
+    transition: width .5s ease;
 }
 </style>
