e80aa8e7a7809b94bb049e7b7d3783e2425502ba
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2021-09-27 17:27:31 +0200

    Optional title

--- web/src/components/BasicModal.vue
+++ web/src/components/BasicModal.vue
@@ -54,7 +54,7 @@
                             </svg>
                         </div>
                         <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                            <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+                            <h3 class="modal-title text-lg leading-6 font-medium text-gray-900" v-if="title">
                             {{ title }}
                             </h3>
                             <div class="mt-2">
