5a583eb5b42b3c524c35fd3794fdaa000e91ad94
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2021-11-22 17:29:08 +0100

    Add FilePicker/FilePreview

---
+++ web/src/components/FilePicker.vue
@@ -0,0 +1,49 @@
+<template>
+    <label>
+        <input type="file" :name="name" :accept="accept" @change="filePicker($event)">
+        <FilePreview :src="imageSrc" />
+    </label>
+</template>
+
+<script>
+import FilePreview from './FilePreview.vue'
+
+export default {
+    components: {
+        FilePreview
+    },
+
+    name: 'FilePicker',
+    props: {
+        name: String,
+        accept: {
+            default: 'image/png, image/jpeg, image/gif',
+            type: String
+        },
+        filePreview: {
+            default: true,
+            type: Boolean
+        }
+    },
+
+    data() {
+        return {
+            imageSrc: ''
+        }
+    },
+
+    methods: {
+        filePicker(event) {
+            let file = event.target.files[0];
+            let imageSrc = URL.createObjectURL(file);
+            this.imageSrc = imageSrc;
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    input[type='file'] {
+        display: none;
+    }
+</style>
---
+++ web/src/components/FilePreview.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="file-preview w-52 h-52 border-2 border-black rounded flex items-center overflow-hidden">
+        <img :src="src" class="block">
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'FilePrview',
+    props: {
+        src: String
+    },
+}
+</script>
+
+
+<style lang="scss" scoped>
+    .file-preview {
+        background-image: url('../assets/img/add.png');
+        background-position: center;
+        background-size: 8rem;
+        background-repeat: no-repeat;
+        cursor: pointer;
+    }
+</style>
