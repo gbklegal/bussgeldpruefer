e875b00ee2ea98005c78c123cd7cec7c34d4abed
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2021-11-22 17:28:15 +0100

    Update DEV.vue

--- web/src/views/DEV.vue
+++ web/src/views/DEV.vue
@@ -1,29 +1,15 @@
 <template>
-    <h1>DEV</h1>
-    <Modal
-        title="Hello World"
-        text="Demo Text. Nice."
-        button="OK"
-        cancel="no"
-        mode="info"
-        :callback="result"
-    />
-    <button @click.prevent="showModal">open modal</button>
+    <div class="container">
+        <h1>DEV</h1>
+        Variable value: <span>{{ tmp }}</span>
+    </div>
 </template>
 
 <script>
-import Modal from '../components/BasicModal.vue';
-
 export default {
-    components: {
-        Modal,
-    },
-    methods: {
-        result: function(confirmation) {
-            console.log(confirmation);
-        },
-        showModal: function() {
-            Modal.methods.fadeIn();
+    data() {
+        return {
+            tmp: 'Hello World'
         }
     }
 }
