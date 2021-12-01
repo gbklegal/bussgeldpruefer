2b4ace93dddf03c557a9624fc661ff708715e8a6
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2021-12-01 17:52:23 +0100

    Update FilePreview.vue

--- web/src/components/FilePreview.vue
+++ web/src/components/FilePreview.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="file-preview w-52 h-52 border-2 border-black rounded flex items-center overflow-hidden">
+    <div class="flex items-center overflow-hidden border-2 border-black rounded file-preview w-52 h-52">
         <img :src="src" class="block">
     </div>
 </template>
@@ -9,18 +9,29 @@
 export default {
     name: 'FilePrview',
     props: {
-        src: String
+        src: String,
     },
 }
 </script>
 
 
 <style lang="scss" scoped>
-    .file-preview {
-        background-image: url('../assets/img/add.png');
-        background-position: center;
-        background-size: 8rem;
-        background-repeat: no-repeat;
-        cursor: pointer;
+.file-preview {
+    position: relative;
+    background-image: url('../assets/img/add.png');
+    background-position: center;
+    background-size: 8rem;
+    background-repeat: no-repeat;
+    cursor: pointer;
+
+    display: inline-flex;
+    align-items: center;
+    overflow: hidden;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
     }
+}
 </style>
