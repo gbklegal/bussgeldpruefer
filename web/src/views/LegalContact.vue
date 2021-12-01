e2f247d7ffea45bde2aec4cedc428a1608a9826f
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2021-12-01 18:02:25 +0100

    Update LegalContact.vue

--- web/src/views/LegalContact.vue
+++ web/src/views/LegalContact.vue
@@ -3,8 +3,8 @@
         <h1>Impressum</h1>
 
         <div class="mb-6">
-            <!--<h2 class="text-xl mb-3">Impressum</h2>-->
-            <h2 class="text-xl mb-3">Sitz der Gesellschaft:</h2>
+            <!--<h2 class="mb-3 text-xl">Impressum</h2>-->
+            <h2 class="mb-3 text-xl">Sitz der Gesellschaft:</h2>
             <p>GDF GmbH</p>
             <p>Kaiserstraße 23</p>
             <p class="mb-6">90403 Nürnberg</p>
@@ -20,35 +20,35 @@
             </div>
             <div class="mb-3">
                 <span class="mr-2">Internet:</span>
-                <a href="https://xn-bussgeldprfer-5ob.com/">www.bussgeldprüfer.com</a>
+                <a href="https://bussgeldpruefer.com/">www.bussgeldpruefer.com</a>
             </div>
             
         </div>
         <div class="mb-6">
-            <h2 class="text-xl mb-3">Geschäftsführer</h2>
+            <h2 class="mb-3 text-xl">Geschäftsführer</h2>
             <p>Die Gesellschaft wird vertreten durch den Geschäftsführer Tobias Gußmann</p>
             <p>Die Gesellschaft ist eine GmbH mit Sitz in Nürnberg</p>
             <p>Handelsregister beim Amtsgericht Nürnberg: HRB 33156 <br>Umsatzsteuernummer: 241/127/20703</p>
             <p>USt-IdNr. DE 308 255 440</p>
         </div>
         <div class="mb-6">
-            <h2 class="text-xl mb-3">Verantwortlich für den Inhalt <span class="text-sm">(nach § 55 Abs. 2 RStV)</span></h2>
+            <h2 class="mb-3 text-xl">Verantwortlich für den Inhalt <span class="text-sm">(nach § 55 Abs. 2 RStV)</span></h2>
             <p>Tobias Gußmann</p>
         </div>
         <div class="mb-6">
-            <h2 class="text-xl mb-3">Online-Streitbeilegung</h2>
+            <h2 class="mb-3 text-xl">Online-Streitbeilegung</h2>
             <p>Die Europäische Kommission stellt eine Plattform für die außergerichtliche Online-Streitbeilegung (OS-Plattform) bereit, die unter www.ec.europa.eu/consumers/odr aufrufbar ist. Unsere E-Mail-Adresse finden Sie in unserem Impressum. Wir sind weder verpflichtet noch bereit, an dem Streitschlichtungsverfahren teilzunehmen.</p>
         </div>
         <p class="mb-4">Datenschutzbeauftragter: Herr Thomas Dillmann</p>
         <p class="mb-4">Kontaktmöglichkeit: dialog@bussgeldpruefer.com</p>
-        <p class="font-bold mb-6">Dieses Impressum gilt auch für die <a href="https://www.facebook.com/bussgeldpruefer/" target="_blank" class="underline">Facebookseite</a> .</p>
+        <p class="mb-6 font-bold">Dieses Impressum gilt auch für die <a href="https://www.facebook.com/bussgeldpruefer/" target="_blank" class="underline">Facebookseite</a> .</p>
 
         <div class="mb-6">
-            <h2 class="text-xl mb-3">Hinweis zum Leistungsschutzrecht</h2>
+            <h2 class="mb-3 text-xl">Hinweis zum Leistungsschutzrecht</h2>
             <p>Ein Zitieren unserer Inhalte und Links auf unsere Seite begrüßen und fördern wir ausdrücklich. Dennoch gilt als Richtlinie: Überschriften plus Anreißer-Texte oder ein Ausschnitt in vergleichbarer Länge dürfen von anderen Webseiten zitiert werden. Suchmaschinen dürfen Textsnippets der Inhalte unserer Seiten in Ergebnislisten darstellen. In diesen Fällen werden wir keine Lizenzgebühren erheben. Eine Übernahme längerer Textpassagen oder sogar ganzer Texte wird nur mit Zustimmung der GDF GmbH möglich sein. In diesem Falle werden Lizenzgebühren erhoben. Gerne können Sie dazu Kontakt mit uns aufnehmen.</p>
         </div>
         <div>
-            <h2 class="text-xl mb-3">Bildnachweise</h2>
+            <h2 class="mb-3 text-xl">Bildnachweise</h2>
             <p>panthermedia.net</p>
             <p>fotolia.de</p>
         </div>
