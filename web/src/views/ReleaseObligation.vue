2f022f417fd5cd355b5661343dc937bc21c5c175
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2021-12-01 18:03:49 +0100

    Create ReleaseObligation.vue

---
+++ web/src/views/ReleaseObligation.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="container">
+        <h1>Entbindung von der Schweigepflicht</h1>
+
+        <p>Hiermit stimme ich zu, dass die GDB L-Tech UG (haftungsbeschränkt) die von mir im Zusammenhang mit dem von mir angegebenen Bußgeldverfahren übermittelten Daten an die zur Prüfung bereite Partner-Anwaltskanzlei weitergibt. Umgekehrt entbinde ich hiermit auch die mit der Prüfung betraute Partner-Anwaltskanzlei von der Schweigepflicht gegenüber der GDB L-Tech UG (haftungsbeschränkt). Die Partner-Anwaltskanzlei darf somit insbesondere das Prüfergebnis und den weiteren Verlauf der Angelegenheit an die GDB L-Tech UG (haftungsbeschränkt) zurückmelden und sich der GDB L-Tech UG (haftungsbeschränkt) zur Kommunikation mit mir bedienen. Ich kann diese Zustimmungen jederzeit mit Wirkung für die Zukunft widerrufen. Mir ist allerdings bewusst, dass die Kommunikation dadurch ggf. erschwert werden kann.</p>
+    </div>
+</template>
