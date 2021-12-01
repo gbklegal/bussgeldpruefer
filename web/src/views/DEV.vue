99878c1c7bb986e67d73550f7b7820d64fdbe18e
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2021-12-01 18:02:06 +0100

    Add router query

--- web/src/views/DEV.vue
+++ web/src/views/DEV.vue
@@ -2,6 +2,11 @@
     <div class="container">
         <h1>DEV</h1>
         Variable value: <span>{{ tmp }}</span>
+
+        <div class="pt-4">
+            <button class="btn-secondary" @click="orderThankYou">order thank you</button>
+        </div>
+
     </div>
 </template>
 
@@ -10,6 +15,18 @@ export default {
     data() {
         return {
             tmp: 'Hello World'
+        }
+    },
+
+    methods: {
+        orderThankYou() {
+            this.$router.push({
+                name: 'order-thank-you',
+                query: {
+                    order_number: '20210003-4231',
+                    p_id: '4231'
+                }
+            });
         }
     }
 }
