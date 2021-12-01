aa8878c46514b483c53904575cbfa8b16a4bf4f1
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2021-12-01 17:55:21 +0100

    Update global.js

--- web/src/global.js
+++ web/src/global.js
@@ -25,9 +25,13 @@ export default {
      */
     linkTo_UnCryptMailto(s) {
         function unCryptMailto(s) {
-            var n = 0; var r = ''; for (var i = 0; i < s.length; i++) {
+            let n = 0;
+            let r = '';
+            for (let i = 0; i < s.length; i++) {
                 n = s.charCodeAt(i);
-                if (n >= 8364) { n = 128; } r += String.fromCharCode(n - (1));
+                if (n >= 8364) {
+                    n = 128; }
+                r += String.fromCharCode(n - (1));
             }
             return r;
         }
